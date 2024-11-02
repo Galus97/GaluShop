b7cf49118e3d9b8e7130638a4d6bfcea9be7457d
Michał Galus <mgalus7@gmail.com>
2024-11-02 17:16:13 +0100

    SCRUM-1 first commit

---
+++ src/main/java/pl/galushop/GaluShop/GaluShopApplication.java
@@ -0,0 +1,13 @@
+package pl.galushop.GaluShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaluShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaluShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/galushop/GaluShop/GaluShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.galushop.GaluShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaluShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
