fa382149bf3dfba0f36e1ee7c14172c96fdfe210
Michał Galus <mgalus7@gmail.com>
2024-11-12 22:56:13 +0100

    SCRUM-1 added productImages entity

---
+++ src/main/java/pl/galushop/GaluShop/entity/ProductImages.java
@@ -0,0 +1,33 @@
+package pl.galushop.GaluShop.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class ProductImages {
+
+    @Id
+    private Long imagesId;
+
+    @NotBlank
+    private String imgSrc;
+
+    @NotBlank
+    private String altImg;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "productId")
+    @JsonIgnore
+    private Product product;
+}
