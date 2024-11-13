7a05ea5b9262fbdf1a0702597ebbd5daa4425501
Michał Galus <mgalus7@gmail.com>
2024-11-13 06:29:33 +0100

    SCRUM-1 added SpringSecurity class

---
+++ src/main/java/pl/galushop/GaluShop/configuration/SpringSecurity.java
@@ -0,0 +1,45 @@
+package pl.galushop.GaluShop.configuration;
+
+import jakarta.servlet.DispatcherType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurity {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize
+                .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+
+        );
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/login", "/register", "/CSS/**", "/js/**", "/images/**", "/webjars/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/panel")
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
