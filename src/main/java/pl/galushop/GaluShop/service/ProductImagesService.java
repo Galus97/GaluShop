6ad2c4558ee1ae610684959ef291cf08c6a7ffa8
michalgalus <galusmichal@outlook.com>
2024-11-14 08:27:41 +0100

    SCRUM-1 added ProductImagesService

---
+++ src/main/java/pl/galushop/GaluShop/service/ProductImagesService.java
@@ -0,0 +1,17 @@
+package pl.galushop.GaluShop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.galushop.GaluShop.entity.ProductImages;
+import pl.galushop.GaluShop.repository.ProductImagesRepository;
+
+@Service
+@RequiredArgsConstructor
+public class ProductImagesService {
+
+    private final ProductImagesRepository productImagesRepository;
+
+    public void saveProductImagesToDatabase(ProductImages productImages){
+        productImagesRepository.save(productImages);
+    }
+}
