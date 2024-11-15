21c1ae93844d2e2c0192f2179682d86db29c898f
michalgalus <galusmichal@outlook.com>
2024-11-15 07:09:29 +0100

    SCRUM-2 added CurrentUser Component

---
+++ src/main/java/pl/galushop/GaluShop/component/CurrentUser.java
@@ -0,0 +1,17 @@
+package pl.galushop.GaluShop.component;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+@Getter
+public class CurrentUser extends User {
+    private final pl.galushop.GaluShop.entity.User user;
+
+    public CurrentUser(String username, String password, java.util.Collection<?
+            extends GrantedAuthority> authorities, pl.galushop.GaluShop.entity.User user){
+
+        super(username, password, authorities);
+        this.user = user;
+    }
+}
