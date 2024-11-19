8e19e9c30ae2a58318b272ed8aa50853ef26ee31
michalgalus <galusmichal@outlook.com>
2024-11-19 22:28:39 +0100

    SCRUM-2 added LoginUser.jsp

--- src/main/java/pl/galushop/GaluShop/controller/LoginUserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/LoginUserController.java
@@ -1,9 +1,9 @@
 package pl.galushop.GaluShop.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class LoginUserController {
     @GetMapping("/login")
     public String loginGet() {
