eb0be4401edaf2ab73e42b135027c0f2d11783db
michalgalus <galusmichal@outlook.com>
2024-12-06 22:53:22 +0100

    SCRUM-2 added Employee entity class

---
+++ src/main/java/pl/galushop/GaluShop/entity/Employee.java
@@ -0,0 +1,41 @@
+package pl.galushop.GaluShop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    @Size(min = 3)
+    private String firstName;
+
+    @Size(min = 3)
+    private String lastName;
+
+    @Size(min = 5)
+    @Column(unique = true)
+    @Email
+    private String email;
+
+    @Size(min = 5)
+    private String password;
+
+    private boolean enabled;
+
+    private String emailCode;
+}
