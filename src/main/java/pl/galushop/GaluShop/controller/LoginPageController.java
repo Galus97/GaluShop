e02ecd612de4080baaa1973378faf9128fe79c47
michalgalus <galusmichal@outlook.com>
2024-12-19 16:53:00 +0100

    SCRUM-2 added LoginProcessingController

--- src/main/java/pl/galushop/GaluShop/controller/LoginPageController.java
+++ src/main/java/pl/galushop/GaluShop/controller/LoginPageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginPageController {
     @GetMapping("/login")
-    public String defaultLogin() {
+    public String showLoginPage() {
         return "login";
     }
 }
