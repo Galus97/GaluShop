b479d4962de4f08a4d42db19655f9d516d393348
michalgalus <galusmichal@outlook.com>
2025-01-07 07:25:32 +0100

    SCRUM-2 added findProductById in ProductService

--- src/main/java/pl/galushop/GaluShop/service/ProductService.java
+++ src/main/java/pl/galushop/GaluShop/service/ProductService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import pl.galushop.GaluShop.entity.Product;
 import pl.galushop.GaluShop.repository.ProductRepository;
 
+import java.util.NoSuchElementException;
+
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -20,6 +22,14 @@ public class ProductService {
     public void deleteProduct(Long id){
         if(productRepository.findByProductId(id) != null){
             productRepository.delete(productRepository.findByProductId(id));
+        }
+    }
+
+    public Product findProductById(Long id){
+        if(id != null && id > 0){
+            return productRepository.findByProductId(id);
+        } else {
+            throw new NoSuchElementException();
         }
     }
 }
