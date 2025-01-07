397bd2ad7ff65bbabbf5a5801454f645f75c2960
michalgalus <galusmichal@outlook.com>
2025-01-07 07:25:52 +0100

    SCRUM-2 added getMap in OrderAddController

--- src/main/java/pl/galushop/GaluShop/controller/OrderAddController.java
+++ src/main/java/pl/galushop/GaluShop/controller/OrderAddController.java
@@ -1,9 +1,36 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.Order;
+import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.service.OrderService;
+import pl.galushop.GaluShop.service.ProductService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderAddController {
+
+    private final OrderService orderService;
+    private final ProductService productService;
+
+    @GetMapping("/addOrder")
+    public String getOrderAdd(){
+        Order order = new Order();
+
+        Product product = productService.findProductById(1L);
+
+        List<Product> products = new ArrayList<>();
+        products.add(product);
+
+        order.setProducts(products);
+
+        orderService.saveOrderToDatabase(order);
+
+        return "success";
+    }
 }
