56d98cbbf879ee749f38a0cf272ced8c917b16d2
michalgalus <galusmichal@outlook.com>
2025-01-08 15:45:32 +0100

    SCRUM-2 deleted OneToMany relation productImages from Product Entity

--- src/main/java/pl/galushop/GaluShop/entity/Product.java
+++ src/main/java/pl/galushop/GaluShop/entity/Product.java
@@ -1,20 +1,15 @@
 package pl.galushop.GaluShop.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-
-import java.util.List;
 
 @Entity
 @Getter
@@ -40,7 +35,4 @@ public class Product {
 
     @Min(1)
     private Integer categoryId;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "product")
-    private List<ProductImages> productImages;
 }
