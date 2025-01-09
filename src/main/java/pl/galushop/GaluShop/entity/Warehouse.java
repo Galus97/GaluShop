59653f483caacdfe1de07593314a5d7256d8a3c0
michalgalus <galusmichal@outlook.com>
2025-01-09 14:53:42 +0100

    SCRUM-2 added Warehouse entity

---
+++ src/main/java/pl/galushop/GaluShop/entity/Warehouse.java
@@ -0,0 +1,28 @@
+package pl.galushop.GaluShop.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Product product;
+
+    @Min(0)
+    private Integer quantity;
+}
