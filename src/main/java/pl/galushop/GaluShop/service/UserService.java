40e8a30c07195701cfff9cf63513b16c1bb38442
michalgalus <galusmichal@outlook.com>
2025-01-12 21:33:10 +0100

    SCRUM-2 added UserService class

---
+++ src/main/java/pl/galushop/GaluShop/service/UserService.java
@@ -0,0 +1,10 @@
+package pl.galushop.GaluShop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
