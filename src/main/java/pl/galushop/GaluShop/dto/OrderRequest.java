9c57a0b42839c7413406be349ca81479e156ee35
michalgalus <galusmichal@outlook.com>
2025-01-12 21:27:36 +0100

    SCRUM-2 added fields to OrderRequest

--- src/main/java/pl/galushop/GaluShop/dto/OrderRequest.java
+++ src/main/java/pl/galushop/GaluShop/dto/OrderRequest.java
@@ -2,6 +2,10 @@ package pl.galushop.GaluShop.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class OrderRequest {
+    private Long userId;
+    private List<Long> productIds;
 }
