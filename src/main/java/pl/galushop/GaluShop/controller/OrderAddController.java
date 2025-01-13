c3d567b544101d1112f1d29b205938b3260ec568
michalgalus <galusmichal@outlook.com>
2025-01-13 14:07:53 +0100

    SCRUM-2 added saveNewOrder method to OrderAddController

--- src/main/java/pl/galushop/GaluShop/controller/OrderAddController.java
+++ src/main/java/pl/galushop/GaluShop/controller/OrderAddController.java
@@ -1,15 +1,20 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.OrderRequest;
 import pl.galushop.GaluShop.entity.Order;
 import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.entity.User;
 import pl.galushop.GaluShop.service.OrderService;
 import pl.galushop.GaluShop.service.ProductService;
+import pl.galushop.GaluShop.service.UserService;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,20 +22,26 @@ public class OrderAddController {
 
     private final OrderService orderService;
     private final ProductService productService;
+    private final UserService userService;
 
-    @GetMapping("/addOrder")
-    public String getOrderAdd() {
+    @PostMapping("/addOrder")
+    public String saveNewOrder(@RequestBody OrderRequest orderRequest) {
+
+        Optional<User> user = userService.getUserById(orderRequest.getUserId());
+
+        if(user.isEmpty()){
+            return "User not found";
+        }
+
+        List<Product> products = orderRequest.getProductIds().stream()
+                .map(productService::findProductById)
+                .collect(Collectors.toList());
+
         Order order = new Order();
-
-        Product product = productService.findProductById(1L);
-
-        List<Product> products = new ArrayList<>();
-        products.add(product);
-
+        order.setUser(user.get());
         order.setProducts(products);
 
         orderService.saveOrderToDatabase(order);
-
-        return "success";
+        return "Order added successfully!";
     }
 }
