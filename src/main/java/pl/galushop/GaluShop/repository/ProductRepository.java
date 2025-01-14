0a62af3738397d7c92cbd68f60acfeed489cf431
michalgalus <galusmichal@outlook.com>
2025-01-14 02:17:58 +0100

    SCRUM-2 added 'optional' in findByProductId in ProductRepository

--- src/main/java/pl/galushop/GaluShop/repository/ProductRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/ProductRepository.java
@@ -3,7 +3,11 @@ package pl.galushop.GaluShop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.galushop.GaluShop.entity.Product;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Product findByProductId(Long id);
+
+    Optional<Product> findByProductId(Long id);
+
 }
