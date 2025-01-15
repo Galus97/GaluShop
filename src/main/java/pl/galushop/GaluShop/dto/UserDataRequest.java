fe5153ea3c95cf568335cc605224b872d97c8b19
michalgalus <galusmichal@outlook.com>
2025-01-15 11:15:26 +0100

    SCRUM-2 added fields in UserDataRequest class

--- src/main/java/pl/galushop/GaluShop/dto/UserDataRequest.java
+++ src/main/java/pl/galushop/GaluShop/dto/UserDataRequest.java
@@ -4,4 +4,11 @@ import lombok.Data;
 
 @Data
 public class UserDataRequest {
+    private String city;
+    private String street;
+    private Integer streetNumber;
+    private Integer apartmentNumber;
+    private String zipCode;
+    private Integer phoneNumber;
+    private Long userId;
 }
