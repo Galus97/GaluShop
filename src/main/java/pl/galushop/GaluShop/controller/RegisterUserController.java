0a4989a27bb3943e5d0cb1f5e9fcda7d741095b2
michalgalus <galusmichal@outlook.com>
2025-01-16 10:38:47 +0100

    SCRUM-2 added saveNewUser method to RegisterUserController

--- src/main/java/pl/galushop/GaluShop/controller/RegisterUserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/RegisterUserController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import pl.galushop.GaluShop.dto.UserRequest;
 import pl.galushop.GaluShop.entity.User;
 import pl.galushop.GaluShop.exception.ValidationException;
 import pl.galushop.GaluShop.service.RegisterUserService;
@@ -39,5 +41,26 @@ public class RegisterUserController {
         }
 
         return "redirect:login";
+    }
+
+    @PostMapping("/registerUser")
+    public String saveNewUser(@RequestBody UserRequest userRequest){
+
+        User user = new User();
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getLastName());
+        user.setEmail(userRequest.getEmail());
+        user.setPassword(userRequest.getPassword());
+
+        try{
+            registerUserService.saveNewUser(user);
+        } catch (ValidationException exception){
+            List<String> errors = exception.getValidationErrors();
+            for (String error : errors) {
+                System.out.println(error);
+            }
+            return "faild";
+        }
+        return "succes";
     }
 }
