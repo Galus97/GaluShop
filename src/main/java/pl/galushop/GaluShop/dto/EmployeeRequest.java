9d7ffb42869a84ba021b1ea0ffe34e0969379a2f
michalgalus <galusmichal@outlook.com>
2025-01-17 17:29:28 +0100

    SCRUM-2 added fields to EmployeeRequest class

--- src/main/java/pl/galushop/GaluShop/dto/EmployeeRequest.java
+++ src/main/java/pl/galushop/GaluShop/dto/EmployeeRequest.java
@@ -4,5 +4,8 @@ import lombok.Data;
 
 @Data
 public class EmployeeRequest {
-
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
