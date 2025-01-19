f5ff6b556bff776cdf1d07ffe8d05e3c886cbac4
michalgalus <galusmichal@outlook.com>
2025-01-19 17:19:04 +0100

    SCRUM-2 added findAllByUser_UserId in OrderRepository

--- src/main/java/pl/galushop/GaluShop/repository/OrderRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/OrderRepository.java
@@ -3,9 +3,10 @@ package pl.galushop.GaluShop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.galushop.GaluShop.entity.Order;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Optional<Order> findByUser_UserId(Long userId);
+    List<Order> findAllByUser_UserId(Long userId);
 }
