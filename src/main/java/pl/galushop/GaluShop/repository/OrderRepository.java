4730ea692e08bb2df80568ef8f9174b32badb8a3
michalgalus <galusmichal@outlook.com>
2025-01-19 17:13:34 +0100

    SCRUM-2 deleted findByUser_UserId in OrderRepository

--- src/main/java/pl/galushop/GaluShop/repository/OrderRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/OrderRepository.java
@@ -3,6 +3,9 @@ package pl.galushop.GaluShop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.galushop.GaluShop.entity.Order;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    Optional<Order> findByUser_UserId(Long userId);
 }
