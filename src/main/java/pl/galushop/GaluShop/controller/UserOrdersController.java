bc3e1da461620a21ab8f33d7a0896b12ebc27fb0
michalgalus <galusmichal@outlook.com>
2025-01-19 17:34:49 +0100

    SCRUM-2 added showAllUserOrders in UserOrdersController

--- src/main/java/pl/galushop/GaluShop/controller/UserOrdersController.java
+++ src/main/java/pl/galushop/GaluShop/controller/UserOrdersController.java
@@ -1,9 +1,20 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.Order;
+import pl.galushop.GaluShop.service.OrderService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class UserOrdersController {
+    private final OrderService orderService;
+
+    @PostMapping("/allUserOrders")
+    public List<Order> showAllUserOrders(){
+        return orderService.getAllOrdersByUser(1L);
+    }
 }
