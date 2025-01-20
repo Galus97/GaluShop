e97810fd7c7653edb74b655613e6b9fe6f00aa0f
michalgalus <galusmichal@outlook.com>
2025-01-20 19:21:33 +0100

    SCRUM-2 added showUserData method to ShowUserDataController

--- src/main/java/pl/galushop/GaluShop/controller/ShowUserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/ShowUserDataController.java
@@ -1,9 +1,18 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.UserData;
+import pl.galushop.GaluShop.service.UserDataService;
 
 @RestController
 @RequiredArgsConstructor
 public class ShowUserDataController {
+    private final UserDataService userDataService;
+
+    @GetMapping("/userData/show")
+    public UserData showUserData(){
+        return userDataService.showUserData(1L);
+    }
 }
