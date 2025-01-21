fdaf4ec372d7825edbc74bdac59e2c67ba4e2a3b
michalgalus <galusmichal@outlook.com>
2025-01-21 13:10:06 +0100

    SCRUM-2 added getMapping showSpecificOrder method to SpecificOrderController

--- src/main/java/pl/galushop/GaluShop/controller/SpecificOrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/SpecificOrderController.java
@@ -1,9 +1,19 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.Order;
+import pl.galushop.GaluShop.service.OrderService;
 
 @RestController
 @RequiredArgsConstructor
 public class SpecificOrderController {
+    private final OrderService orderService;
+
+
+    @GetMapping("/specificOrder")
+    public Order showSpecificOrder(){
+        return orderService.showSpecificOrder(1L);
+    }
 }
