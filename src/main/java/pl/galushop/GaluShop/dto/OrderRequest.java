704fd89103e013491db3d4975c95bc8c54716d9b
michalgalus <galusmichal@outlook.com>
2025-01-22 10:48:15 +0100

    SCRUM-2 added LocalDateTime field to OrderRequest

--- src/main/java/pl/galushop/GaluShop/dto/OrderRequest.java
+++ src/main/java/pl/galushop/GaluShop/dto/OrderRequest.java
@@ -2,10 +2,12 @@ package pl.galushop.GaluShop.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class OrderRequest {
     private Long userId;
+    private LocalDateTime localDateTime;
     private List<Long> productIds;
 }
