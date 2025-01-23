41a14d54c29217c4bc8d6b10483ec4af118a586b
michalgalus <galusmichal@outlook.com>
2025-01-23 09:29:15 +0100

    SCRUM-2 added deleteUser method to DeleteUserController

--- src/main/java/pl/galushop/GaluShop/controller/DeleteUserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/DeleteUserController.java
@@ -1,9 +1,25 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 public class DeleteUserController {
+        private final UserService userService;
+    @GetMapping("/deleteUser")
+    public String deleteUser(@RequestBody Long userId){
+        if(userId == null || userId <= 0){
+            return "User ID is not valid";
+        }
+        return userService.getUserById(userId)
+                .map(user -> {
+                    userService.deleteUserFromDatabase(user);
+                    return "User has been deleted";
+                })
+                .orElse("User not found");
+    }
 }
