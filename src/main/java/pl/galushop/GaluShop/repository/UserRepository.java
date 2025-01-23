f8a6dd95b6100b85f4c052789a6c2ba4439ca2a0
michalgalus <galusmichal@outlook.com>
2025-01-24 00:43:36 +0100

    SCRUM-2 added updateAllByUserId method to UserRepository

--- src/main/java/pl/galushop/GaluShop/repository/UserRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUserId(Long userId);
+
+    void updateAllByUserId(Long userId);
 }
