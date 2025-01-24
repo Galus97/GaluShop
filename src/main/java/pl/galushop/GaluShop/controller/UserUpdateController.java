029faa8ee8ce5bb0cf1f4fd6fc7380d705de8782
michalgalus <galusmichal@outlook.com>
2025-01-24 01:08:46 +0100

    SCRUM-2 added @GetMapping updateUserFields UserUpdateController

--- src/main/java/pl/galushop/GaluShop/controller/UserUpdateController.java
+++ src/main/java/pl/galushop/GaluShop/controller/UserUpdateController.java
@@ -1,9 +1,35 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.UserRequest;
+import pl.galushop.GaluShop.entity.User;
+import pl.galushop.GaluShop.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 public class UserUpdateController {
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @GetMapping("/updateUser")
+    public String updateUserFields(@RequestBody UserRequest userRequest){
+        if (userRequest.getUserId() == null) {
+            return "User ID is required";
+        }
+
+        User user = new User();
+        user.setUserId(Long.valueOf(userRequest.getUserId()));
+        user.setFirstName(userRequest.getFirstName());
+        user.setLastName(userRequest.getLastName());
+        user.setEmail(userRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(userRequest.getFirstName()));
+
+        userService.updateUser(user.getUserId(), user);
+
+        return "Success";
+    }
 }
