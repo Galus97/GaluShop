dfaede5235395aea9c9b5e12052629c04f50627f
michalgalus <galusmichal@outlook.com>
2025-01-25 10:17:19 +0100

    SCRUM-2 added CorsConfig class

---
+++ src/main/java/pl/galushop/GaluShop/configuration/CorsConfig.java
@@ -0,0 +1,6 @@
+package pl.galushop.GaluShop.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig  implements WebMvcConfigurer {
+}
