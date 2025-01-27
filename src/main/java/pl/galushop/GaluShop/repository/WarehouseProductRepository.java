7d3b423bd055cc17ef4b5175f3b47eea47bcd880
michalgalus <galusmichal@outlook.com>
2025-01-27 15:38:45 +0100

    SCRUM-2 added findByProduct_ProductId to WarehouseProductRepository

--- src/main/java/pl/galushop/GaluShop/repository/WarehouseProductRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/WarehouseProductRepository.java
@@ -5,4 +5,5 @@ import pl.galushop.GaluShop.entity.WarehouseProduct;
 
 public interface WarehouseProductRepository extends JpaRepository<WarehouseProduct, Long> {
 
+    WarehouseProduct findByProduct_ProductId(Long productId);
 }
