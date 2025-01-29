193452a1a2bfa0dea76e5efa74235bd4414694b2
michalgalus <galusmichal@outlook.com>
2025-01-29 23:11:59 +0100

    SCRUM-2 added @Setter(AccessLevel.NONE) in WarehouseProduct Product class

--- src/main/java/pl/galushop/GaluShop/entity/Product.java
+++ src/main/java/pl/galushop/GaluShop/entity/Product.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -19,6 +20,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long productId;
 
     @Size(min = 3)
