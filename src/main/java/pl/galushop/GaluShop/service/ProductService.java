d91bf877c972c9ce7d98dcdc6426e69c429c0bc3
michalgalus <galusmichal@outlook.com>
2025-01-30 11:49:13 +0100

    SCRUM-2 added updateProductByProductId to ProductService

--- src/main/java/pl/galushop/GaluShop/service/ProductService.java
+++ src/main/java/pl/galushop/GaluShop/service/ProductService.java
@@ -38,4 +38,21 @@ public class ProductService {
         }
     }
 
+    public void updateProductByProductId(Long productId, String productName, String description,
+                                         Double price, String category, Integer categoryId){
+        if(productId > 0 && productRepository.findByProductId(productId).isPresent()){
+            if(checkFields(productName, description, price, category, categoryId)){
+                productRepository.updateProductByProductId(productId, productName, description, price, category, categoryId);
+            }
+           throw new IllegalArgumentException("Some fields are invalid");
+        }
+        throw new IllegalArgumentException("Product Id is invalid");
+    }
+
+    private boolean checkFields(String productName, String description, Double price, String category, Integer categoryId){
+        if(productName.isBlank() || description.isBlank() || price < 0 || category.isBlank() || categoryId < 0){
+            return false;
+        }
+        return true;
+    }
 }
