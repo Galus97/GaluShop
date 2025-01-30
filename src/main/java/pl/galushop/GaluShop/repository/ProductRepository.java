da8409db07425fec6203415b5372bcf85fa2307a
michalgalus <galusmichal@outlook.com>
2025-01-30 10:51:07 +0100

    SCRUM-2 added updateProductByProductId to ProductRepository

--- src/main/java/pl/galushop/GaluShop/repository/ProductRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/ProductRepository.java
@@ -1,6 +1,9 @@
 package pl.galushop.GaluShop.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import pl.galushop.GaluShop.entity.Product;
 
 import java.util.Optional;
@@ -10,4 +13,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     Optional<Product> findByProductId(Long id);
 
+    @Transactional
+    @Modifying
+    @Query("UPDATE Product p SET p.productName = :productName, p.description = : description, p.price = :price, " +
+            "p.category = :category, p.categoryId = : categoryId WHERE p.productId = :productId")
+    void updateProductByProductId(Long productId, String productName, String description,
+                                  Double price, String category, Integer categoryId);
 }
