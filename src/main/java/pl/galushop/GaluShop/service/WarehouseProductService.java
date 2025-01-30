d94add011559f7452d2ba0a478981ccd4fd8b856
michalgalus <galusmichal@outlook.com>
2025-01-30 10:49:11 +0100

    SCRUM-2 added updateQuantityByProductId to WarehouseProductService

--- src/main/java/pl/galushop/GaluShop/service/WarehouseProductService.java
+++ src/main/java/pl/galushop/GaluShop/service/WarehouseProductService.java
@@ -15,29 +15,41 @@ public class WarehouseProductService {
     private final WarehouseProductRepository warehouseRepository;
     private final ProductRepository productRepository;
 
-    public void addProductToWarehouse(WarehouseProduct warehouseProduct){
-        if(warehouseProduct != null){
+    public void addProductToWarehouse(WarehouseProduct warehouseProduct) {
+        if (warehouseProduct != null) {
             warehouseRepository.save(warehouseProduct);
         }
     }
 
-    public List<WarehouseProduct> getAllProductInWarehouse(){
+    public List<WarehouseProduct> getAllProductInWarehouse() {
         return warehouseRepository.findAll();
     }
 
-    public void deleteWarehouseProduct(Long warehouseId){
-        if(warehouseId != null && warehouseId > 0){
+    public void deleteWarehouseProduct(Long warehouseId) {
+        if (warehouseId != null && warehouseId > 0) {
             warehouseRepository.deleteById(warehouseId);
         }
     }
 
-    public WarehouseProduct getProductInfoInWarehouse(Long productId){
-        if(productId != null && productId > 0) {
-            if(productRepository.findByProductId(productId).isPresent()){
+    public WarehouseProduct getProductInfoInWarehouse(Long productId) {
+        if (productId != null && productId > 0) {
+            if (productRepository.findByProductId(productId).isPresent()) {
                 return warehouseRepository.findByProduct_ProductId(productId);
             }
             throw new NoSuchElementException("That product doesn't exist in database");
         }
         throw new IllegalArgumentException("Product Id is invalid");
+    }
+
+    public void updateQuantityByProductId(Long productId, Integer quantity) {
+        if (productId != null && productId > 0 && quantity != null && quantity >= 0) {
+            if (productRepository.findByProductId(productId).isPresent()) {
+                warehouseRepository.updateQuantityByProductId(productId, quantity);
+            } else {
+                throw new NoSuchElementException("That product doesn't exist in database");
+            }
+        } else {
+            throw new IllegalArgumentException("Product Id is invalid");
+        }
     }
 }
