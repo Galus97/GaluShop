b8412a2c13b01f1a3c46236d3f1e76baa2ad088f
michalgalus <galusmichal@outlook.com>
2025-01-30 10:44:58 +0100

    SCRUM-2 added updateQuantityByProductId to WarehouseProductRepository

--- src/main/java/pl/galushop/GaluShop/repository/WarehouseProductRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/WarehouseProductRepository.java
@@ -1,9 +1,17 @@
 package pl.galushop.GaluShop.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import pl.galushop.GaluShop.entity.WarehouseProduct;
 
 public interface WarehouseProductRepository extends JpaRepository<WarehouseProduct, Long> {
 
     WarehouseProduct findByProduct_ProductId(Long productId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE WarehouseProduct wp SET wp.quantity = :quantity WHERE wp.product.productId = :productId")
+    void updateQuantityByProductId(Long productId, Integer quantity);
 }
