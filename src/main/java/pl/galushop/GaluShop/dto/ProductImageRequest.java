e707362c293314c0f94977357bc302cce99e7397
michalgalus <galusmichal@outlook.com>
2025-01-31 12:08:02 +0100

    SCRUM-2 added imagesId to ProductImageRequest

--- src/main/java/pl/galushop/GaluShop/dto/ProductImageRequest.java
+++ src/main/java/pl/galushop/GaluShop/dto/ProductImageRequest.java
@@ -1,9 +1,12 @@
 package pl.galushop.GaluShop.dto;
 
 import lombok.Data;
+import pl.galushop.GaluShop.entity.Product;
 
 @Data
 public class ProductImageRequest {
+    private Long imagesId;
     private String imgSrc;
     private String altImg;
+    private Product product;
 }
