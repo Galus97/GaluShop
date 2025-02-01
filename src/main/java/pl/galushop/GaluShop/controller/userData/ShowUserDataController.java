daa8fe58e372c7bf96ab2575b997d478f37e6fd3
michalgalus <galusmichal@outlook.com>
2025-02-01 21:30:40 +0100

    SCRUM-2 added userDataController package

--- src/main/java/pl/galushop/GaluShop/controller/userData/ShowUserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/userData/ShowUserDataController.java
@@ -1,4 +1,4 @@
-package pl.galushop.GaluShop.controller;
+package pl.galushop.GaluShop.controller.userData;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
