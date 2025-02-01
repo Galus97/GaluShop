718a3938fed7a025508767ff15af3e970c4f8585
michalgalus <galusmichal@outlook.com>
2025-02-01 21:31:28 +0100

    SCRUM-2 added employeeController package

--- src/main/java/pl/galushop/GaluShop/controller/employee/EmployeePanelController.java
+++ src/main/java/pl/galushop/GaluShop/controller/employee/EmployeePanelController.java
@@ -1,4 +1,4 @@
-package pl.galushop.GaluShop.controller;
+package pl.galushop.GaluShop.controller.employee;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
