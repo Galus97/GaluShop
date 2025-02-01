589b8851b753b05146e9a8d84e95a0280b869504
michalgalus <galusmichal@outlook.com>
2025-02-01 21:33:05 +0100

    SCRUM-2 added warehouseController package

--- src/main/java/pl/galushop/GaluShop/controller/warehouse/AllWarehouseProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/warehouse/AllWarehouseProductController.java
@@ -1,4 +1,4 @@
-package pl.galushop.GaluShop.controller;
+package pl.galushop.GaluShop.controller.warehouse;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
