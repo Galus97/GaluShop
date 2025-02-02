d3799c97f95a8e660602405907a953e6a1ebcc65
michalgalus <galusmichal@outlook.com>
2025-02-02 21:56:04 +0100

    SCRUM-2 added findAllByProduct_ProductId to ProductImagesRepository

--- src/main/java/pl/galushop/GaluShop/repository/ProductImagesRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/ProductImagesRepository.java
@@ -3,5 +3,9 @@ package pl.galushop.GaluShop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.galushop.GaluShop.entity.ProductImages;
 
+import java.util.List;
+
 public interface ProductImagesRepository extends JpaRepository<ProductImages, Long> {
+
+    List<ProductImages> findAllByProduct_ProductId(Long productId);
 }
