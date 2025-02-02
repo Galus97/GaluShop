9fa03747641c668aecebffd240b709b6321980e5
michalgalus <galusmichal@outlook.com>
2025-02-02 21:44:29 +0100

    SCRUM-2 added deleteProductImages to ProductImagesService

--- src/main/java/pl/galushop/GaluShop/service/ProductImagesService.java
+++ src/main/java/pl/galushop/GaluShop/service/ProductImagesService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import pl.galushop.GaluShop.entity.ProductImages;
 import pl.galushop.GaluShop.repository.ProductImagesRepository;
 
+import java.util.NoSuchElementException;
+
 @Service
 @RequiredArgsConstructor
 public class ProductImagesService {
@@ -13,5 +15,15 @@ public class ProductImagesService {
 
     public void saveProductImagesToDatabase(ProductImages productImages) {
         productImagesRepository.save(productImages);
+    }
+
+    public void deleteProductImages(Long imagesId){
+        if(imagesId != null && imagesId > 0){
+            if(productImagesRepository.existsById(imagesId)){
+                productImagesRepository.deleteById(imagesId);
+            }
+            throw new NoSuchElementException("That Image doesn't exist in database");
+        }
+        throw new IllegalArgumentException("Image Product Id is invalid");
     }
 }
