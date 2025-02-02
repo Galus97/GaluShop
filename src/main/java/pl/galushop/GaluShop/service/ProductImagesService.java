7b8da28cff2fc1019c1a668bdb6475108d79a10f
michalgalus <galusmichal@outlook.com>
2025-02-02 22:07:39 +0100

    SCRUM-2 added getAllImagesByProductId to ProductImagesService

--- src/main/java/pl/galushop/GaluShop/service/ProductImagesService.java
+++ src/main/java/pl/galushop/GaluShop/service/ProductImagesService.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.galushop.GaluShop.entity.ProductImages;
 import pl.galushop.GaluShop.repository.ProductImagesRepository;
+import pl.galushop.GaluShop.repository.ProductRepository;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
@@ -21,9 +23,21 @@ public class ProductImagesService {
         if(imagesId != null && imagesId > 0){
             if(productImagesRepository.existsById(imagesId)){
                 productImagesRepository.deleteById(imagesId);
+            } else {
+                throw new NoSuchElementException("That Image doesn't exist in database");
             }
-            throw new NoSuchElementException("That Image doesn't exist in database");
+        } else {
+            throw new IllegalArgumentException("Image Product Id is invalid");
         }
-        throw new IllegalArgumentException("Image Product Id is invalid");
+    }
+
+    public List<ProductImages> getAllImagesByProductId(Long productId){
+        if(productId != null && productId > 0){
+            if(productImagesRepository.existsByProduct_ProductId(productId)){
+                return productImagesRepository.findAllByProduct_ProductId(productId);
+            }
+            throw new NoSuchElementException("Images doesn't exist in database");
+        }
+        throw new IllegalArgumentException("Product Id is invalid");
     }
 }
