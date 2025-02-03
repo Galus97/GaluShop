b99b418f2c67a02db7ca02b76dfb65fab3e7c260
michalgalus <galusmichal@outlook.com>
2025-02-03 12:36:50 +0100

    SCRUM-2 changed method to delete warehouseProduct

--- src/main/java/pl/galushop/GaluShop/controller/warehouse/DeleteWarehouseProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/warehouse/DeleteWarehouseProductController.java
@@ -1,9 +1,19 @@
 package pl.galushop.GaluShop.controller.warehouse;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.WarehouseProductRequest;
+import pl.galushop.GaluShop.service.WarehouseProductService;
 
 @RestController
 @RequiredArgsConstructor
 public class DeleteWarehouseProductController {
+    private final WarehouseProductService warehouseProductService;
+
+    @GetMapping("/deleteWarehouseProduct")
+    public String deleteWarehouseProduct(@RequestBody WarehouseProductRequest warehouseProductRequest){
+        warehouseProductRequest.g
+    }
 }
