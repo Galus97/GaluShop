ef2f64104d4c1816b9629aa98f4a3f8ee2f84bf9
michalgalus <galusmichal@outlook.com>
2025-02-03 13:27:09 +0100

    SCRUM-2 added showInfoWarehouseProduct to WarehouseProductInfoController

--- src/main/java/pl/galushop/GaluShop/controller/warehouse/WarehouseProductInfoController.java
+++ src/main/java/pl/galushop/GaluShop/controller/warehouse/WarehouseProductInfoController.java
@@ -1,9 +1,20 @@
 package pl.galushop.GaluShop.controller.warehouse;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.WarehouseProductRequest;
+import pl.galushop.GaluShop.entity.WarehouseProduct;
+import pl.galushop.GaluShop.service.WarehouseProductService;
 
 @RestController
 @RequiredArgsConstructor
 public class WarehouseProductInfoController {
+    private final WarehouseProductService warehouseProductService;
+
+    @GetMapping("/showInfoWarehouseProduct")
+    public WarehouseProduct showInfoWarehouseProduct(@RequestBody WarehouseProductRequest warehouseProductRequest){
+        return warehouseProductService.getProductInfoInWarehouse(warehouseProductRequest.getProductId());
+    }
 }
