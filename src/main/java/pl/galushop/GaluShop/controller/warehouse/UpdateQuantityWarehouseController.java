6ff314397069c55a81cfa992eaa568b6847c24da
michalgalus <galusmichal@outlook.com>
2025-02-03 13:18:35 +0100

    SCRUM-2 added updateQuantity to UpdateQuantityWarehouseController

--- src/main/java/pl/galushop/GaluShop/controller/warehouse/UpdateQuantityWarehouseController.java
+++ src/main/java/pl/galushop/GaluShop/controller/warehouse/UpdateQuantityWarehouseController.java
@@ -1,9 +1,20 @@
 package pl.galushop.GaluShop.controller.warehouse;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.WarehouseProductRequest;
+import pl.galushop.GaluShop.service.WarehouseProductService;
 
 @RestController
 @RequiredArgsConstructor
 public class UpdateQuantityWarehouseController {
+    private final WarehouseProductService warehouseProductService;
+    @GetMapping("/updateQuantity")
+    public String updateQuantity(@RequestBody WarehouseProductRequest warehouseProductRequest){
+        warehouseProductService.updateQuantityByProductId(warehouseProductRequest.getProductId(),
+                warehouseProductRequest.getQuantity());
+        return "Success";
+    }
 }
