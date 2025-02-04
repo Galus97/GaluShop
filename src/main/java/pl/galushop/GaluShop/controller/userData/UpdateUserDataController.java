8103aec152953bcb9395c9d9a0e6504ecc95857d
michalgalus <galusmichal@outlook.com>
2025-02-04 11:47:34 +0100

    SCRUM-2 added @GetMapping updateUserData to UpdateUserDataController

--- src/main/java/pl/galushop/GaluShop/controller/userData/UpdateUserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/userData/UpdateUserDataController.java
@@ -1,10 +1,30 @@
 package pl.galushop.GaluShop.controller.userData;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.UserDataRequest;
+import pl.galushop.GaluShop.entity.UserData;
+import pl.galushop.GaluShop.service.UserDataService;
 
 @RestController
 @RequiredArgsConstructor
 public class UpdateUserDataController {
+    private final UserDataService userDataService;
+
+    @GetMapping("/userData/update")
+    public UserData updateUserData(@RequestBody UserDataRequest userDataRequest){
+        userDataService.updateUserData(
+                userDataRequest.getUserDataId(),
+                userDataRequest.getCity(),
+                userDataRequest.getStreet(),
+                userDataRequest.getStreetNumber(),
+                userDataRequest.getApartmentNumber(),
+                userDataRequest.getZipCode(),
+                userDataRequest.getPhoneNumber()
+        );
+
+        return userDataService.showUserData(userDataRequest.getUserDataId());
+    }
 }
