7ec7c435e998445510c43e3bc8a506df01f925cd
michalgalus <galusmichal@outlook.com>
2025-02-04 12:11:16 +0100

    SCRUM-2 added deleteUserData to UserDataService

--- src/main/java/pl/galushop/GaluShop/service/UserDataService.java
+++ src/main/java/pl/galushop/GaluShop/service/UserDataService.java
@@ -20,9 +20,9 @@ public class UserDataService {
         }
     }
 
-    public UserData showUserData(Long userDataId){
-        if(userDataId != null && userDataId > 0){
-            if(userDataRepository.findById(userDataId).isPresent()){
+    public UserData showUserData(Long userDataId) {
+        if (userDataId != null && userDataId > 0) {
+            if (userDataRepository.findById(userDataId).isPresent()) {
                 return userDataRepository.findById(userDataId).get();
             }
             throw new NoSuchElementException("That User Data doesn't exist in database");
@@ -30,9 +30,9 @@ public class UserDataService {
         throw new IllegalArgumentException("User Data Id is invalid");
     }
 
-    public UserData showUserDataByUserId(Long userId){
-        if(userId != null && userId > 0){
-            if(userService.getUserById(userId).isPresent()){
+    public UserData showUserDataByUserId(Long userId) {
+        if (userId != null && userId > 0) {
+            if (userService.getUserById(userId).isPresent()) {
                 return userDataRepository.findByUser_UserId(userId);
             }
             throw new NoSuchElementException("That User Data doesn't exist in database");
@@ -41,11 +41,23 @@ public class UserDataService {
     }
 
     public void updateUserData(Long userDataId, String city, String street, Integer streetNumber,
-                               Integer apartmentNumber, String zipCode, Integer phoneNumber){
-        if(userDataId != null && userDataId > 0){
-            if(userDataRepository.existsById(userDataId)){
+                               Integer apartmentNumber, String zipCode, Integer phoneNumber) {
+        if (userDataId != null && userDataId > 0) {
+            if (userDataRepository.existsById(userDataId)) {
                 userDataRepository.updateByUserDataId(userDataId, city, street, streetNumber,
                         apartmentNumber, zipCode, phoneNumber);
+            } else {
+                throw new NoSuchElementException("That User Data doesn't exist in database");
+            }
+        } else {
+            throw new IllegalArgumentException("User Data Id is invalid");
+        }
+    }
+
+    public void deleteUserData(Long userDataId) {
+        if (userDataId != null && userDataId > 0) {
+            if (userDataRepository.existsById(userDataId)) {
+                userDataRepository.deleteById(userDataId);
             } else {
                 throw new NoSuchElementException("That User Data doesn't exist in database");
             }
