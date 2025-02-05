2f6acaaa6c4d7e6b5f8111345c21584417f89dc6
michalgalus <galusmichal@outlook.com>
2025-02-05 13:24:09 +0100

    SCRUM-2 added showUserInfo to ShowUserController

--- src/main/java/pl/galushop/GaluShop/controller/user/ShowUserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/user/ShowUserController.java
@@ -1,9 +1,21 @@
 package pl.galushop.GaluShop.controller.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.UserRequest;
+import pl.galushop.GaluShop.entity.User;
+import pl.galushop.GaluShop.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 public class ShowUserController {
+    private final UserService userService;
+
+    @GetMapping("/user/show")
+    public User showUserInfo(@RequestBody UserRequest userRequest){
+       return userService.getUserById(userRequest.getUserId());
+    }
 }
+
