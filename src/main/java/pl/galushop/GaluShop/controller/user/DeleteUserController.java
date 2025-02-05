fd5a553e0707afc4e00c2adf084c998a476d8a02
michalgalus <galusmichal@outlook.com>
2025-02-05 13:32:34 +0100

    SCRUM-2 used UserRequest instead of userId in DeleteUserController

--- src/main/java/pl/galushop/GaluShop/controller/user/DeleteUserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/user/DeleteUserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.UserRequest;
 import pl.galushop.GaluShop.service.UserService;
 
 @RestController
@@ -11,15 +12,8 @@ import pl.galushop.GaluShop.service.UserService;
 public class DeleteUserController {
         private final UserService userService;
     @GetMapping("/deleteUser")
-    public String deleteUser(@RequestBody Long userId){
-        if(userId == null || userId <= 0){
-            return "User ID is not valid";
-        }
-        return userService.getUserById(userId)
-                .map(user -> {
-                    userService.deleteUserFromDatabase(user);
-                    return "User has been deleted";
-                })
-                .orElse("User not found");
+    public String deleteUser(@RequestBody UserRequest userRequest){
+        userService.getUserById(userRequest.getUserId());
+        return "Success";
     }
 }
