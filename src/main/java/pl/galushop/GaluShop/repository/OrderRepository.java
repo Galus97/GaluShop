8dd513e740f5dfabf4552974493c936f0f5be754
michalgalus <galusmichal@outlook.com>
2025-02-06 18:13:21 +0100

    SCRUM-2 added updateOrderByOrderId method to OrderRepository

--- src/main/java/pl/galushop/GaluShop/repository/OrderRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/OrderRepository.java
@@ -1,8 +1,14 @@
 package pl.galushop.GaluShop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+import pl.galushop.GaluShop.component.OrderStatus;
 import pl.galushop.GaluShop.entity.Order;
+import pl.galushop.GaluShop.entity.Product;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,4 +17,9 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByUser_UserId(Long userId);
 
     Optional<Order> findByUser_UserId(Long userId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Order o SET o.localDateTime = :localDateTime, o.status = :status, o.products = :products WHERE o.orderId = :orderId")
+    void updateOrderByOrderId(Long orderId, LocalDateTime localDateTime, OrderStatus status, List<Product> products);
 }
