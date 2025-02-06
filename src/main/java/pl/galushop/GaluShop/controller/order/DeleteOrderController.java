a042bb3b7180151fd503f6d1c29315452d33bc4f
michalgalus <galusmichal@outlook.com>
2025-02-06 18:08:38 +0100

    SCRUM-2 added deleteOrder method to DeleteOrderController

--- src/main/java/pl/galushop/GaluShop/controller/order/DeleteOrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/order/DeleteOrderController.java
@@ -1,10 +1,21 @@
 package pl.galushop.GaluShop.controller.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.OrderRequest;
+import pl.galushop.GaluShop.entity.Order;
+import pl.galushop.GaluShop.service.OrderService;
 
 @RestController
 @RequiredArgsConstructor
 public class DeleteOrderController {
+    private final OrderService orderService;
 
+    @GetMapping("/deleteOrder")
+    public String deleteOrder(@RequestBody OrderRequest orderRequest){
+        orderService.deleteOrder(orderRequest.getUserId());
+        return "Success";
+    }
 }
