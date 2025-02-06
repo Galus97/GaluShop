318c383cbd26f631f89a8dfcc8ab40fa1ffc9c4a
michalgalus <galusmichal@outlook.com>
2025-02-06 18:27:09 +0100

    SCRUM-2 added deleteEmployee method to  EmployeeService

--- src/main/java/pl/galushop/GaluShop/service/EmployeeService.java
+++ src/main/java/pl/galushop/GaluShop/service/EmployeeService.java
@@ -1,7 +1,25 @@
 package pl.galushop.GaluShop.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.galushop.GaluShop.repository.EmployeeRepository;
+
+import java.util.NoSuchElementException;
 
 @Service
+@RequiredArgsConstructor
 public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public void deleteEmployee(Long employeeId){
+        if(employeeId != null && employeeId > 0){
+            if(employeeRepository.findById(employeeId).isPresent()){
+                employeeRepository.deleteById(employeeId);
+            } else {
+                throw new NoSuchElementException("This employee doesn't exist in database");
+            }
+        } else {
+            throw new IllegalArgumentException("Employee Id is invalid");
+        }
+    }
 }
