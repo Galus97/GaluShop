e866475b25946ac0c7f24fb443c84ade2805c6c9
michalgalus <galusmichal@outlook.com>
2025-02-07 21:24:03 +0100

    SCRUM-2 added UserNotFoundException

---
+++ src/main/java/pl/galushop/GaluShop/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package pl.galushop.GaluShop.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserNotFoundException {
+}
