b416dd0d6e310641a1d403d69c09040fedf14efa
michalgalus <galusmichal@outlook.com>
2025-02-07 19:50:41 +0100

    SCRUM-2 added showEmployeeInfo to ShowEmployeeController

--- src/main/java/pl/galushop/GaluShop/controller/employee/ShowEmployeeController.java
+++ src/main/java/pl/galushop/GaluShop/controller/employee/ShowEmployeeController.java
@@ -1,9 +1,20 @@
 package pl.galushop.GaluShop.controller.employee;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.EmployeeRequest;
+import pl.galushop.GaluShop.entity.Employee;
+import pl.galushop.GaluShop.service.EmployeeService;
 
 @RestController
 @RequiredArgsConstructor
 public class ShowEmployeeController {
+    private final EmployeeService employeeService;
+
+    @GetMapping("/employee/show")
+    public Employee showEmployeeInfo(@RequestBody EmployeeRequest employeeRequest){
+        return employeeService.showEmployeeInf(employeeRequest.getEmployeeId());
+    }
 }
