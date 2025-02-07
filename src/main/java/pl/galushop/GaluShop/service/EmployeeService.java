fe9b564db95c471077352b8104425e80d746c074
michalgalus <galusmichal@outlook.com>
2025-02-07 19:47:52 +0100

    SCRUM-2 added showEmployeeInf to EmployeeService

--- src/main/java/pl/galushop/GaluShop/service/EmployeeService.java
+++ src/main/java/pl/galushop/GaluShop/service/EmployeeService.java
@@ -2,6 +2,7 @@ package pl.galushop.GaluShop.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.galushop.GaluShop.entity.Employee;
 import pl.galushop.GaluShop.repository.EmployeeRepository;
 
 import java.util.NoSuchElementException;
@@ -13,7 +14,7 @@ public class EmployeeService {
 
     public void deleteEmployee(Long employeeId){
         if(employeeId != null && employeeId > 0){
-            if(employeeRepository.findById(employeeId).isPresent()){
+            if(employeeRepository.existsById(employeeId)){
                 employeeRepository.deleteById(employeeId);
             } else {
                 throw new NoSuchElementException("This employee doesn't exist in database");
@@ -25,7 +26,7 @@ public class EmployeeService {
 
     public void updateEmployee(Long employeeId, String firstName, String lastName, String email, String password){
         if(employeeId != null && employeeId > 0) {
-            if (employeeRepository.findById(employeeId).isPresent()) {
+            if (employeeRepository.existsById(employeeId)) {
                 employeeRepository.updateEmployeeByEmployeeId(employeeId, firstName, lastName, email, password);
             } else {
                 throw new NoSuchElementException("This employee doesn't exist in database");
@@ -33,5 +34,15 @@ public class EmployeeService {
         } else {
             throw new IllegalArgumentException("Employee Id is invalid");
         }
+    }
+
+    public Employee showEmployeeInf(Long employeeId){
+        if(employeeId != null && employeeId > 0){
+            if(employeeRepository.existsById(employeeId)){
+                return employeeRepository.findById(employeeId).get();
+            }
+            throw new NoSuchElementException("This employee doesn't exist in database");
+        }
+        throw new IllegalArgumentException("Employee Id is invalid");
     }
 }
