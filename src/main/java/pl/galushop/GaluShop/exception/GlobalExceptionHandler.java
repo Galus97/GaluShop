abc2cc7dcc1bcd1c14ed320635f1256b750307d4
michalgalus <galusmichal@outlook.com>
2025-02-07 21:30:20 +0100

    SCRUM-2 added handleUserNotFoundException to  UserNotFoundException

--- src/main/java/pl/galushop/GaluShop/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/galushop/GaluShop/exception/GlobalExceptionHandler.java
@@ -1,7 +1,18 @@
 package pl.galushop.GaluShop.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    public ResponseEntity<Map<String, String>> handleUserNotFoundException(UserNotFoundException e){
+        Map<String, String> response = new HashMap<>();
+        response.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 }
