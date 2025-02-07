268d864e5ef60d4a8de59d87f97f53b63895a486
michalgalus <galusmichal@outlook.com>
2025-02-07 21:24:58 +0100

    SCRUM-2 added UserNotFoundErrors list to  UserNotFoundException

--- src/main/java/pl/galushop/GaluShop/exception/UserNotFoundException.java
+++ src/main/java/pl/galushop/GaluShop/exception/UserNotFoundException.java
@@ -3,7 +3,10 @@ package pl.galushop.GaluShop.exception;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
 public class UserNotFoundException {
+    private final List<String> UserNotFoundErrors;
 }
