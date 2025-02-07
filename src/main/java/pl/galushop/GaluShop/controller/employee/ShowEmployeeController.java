e4e008b4d75bad98b89c4ac0b05275db913ee275
michalgalus <galusmichal@outlook.com>
2025-02-07 19:48:28 +0100

    SCRUM-2 added ShowEmployeeController

---
+++ src/main/java/pl/galushop/GaluShop/controller/employee/ShowEmployeeController.java
@@ -0,0 +1,9 @@
+package pl.galushop.GaluShop.controller.employee;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ShowEmployeeController {
+}
