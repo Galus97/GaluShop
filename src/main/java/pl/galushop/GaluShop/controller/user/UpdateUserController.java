0622d9e560c1c7685aaeb1157d82de5d7fee7511
michalgalus <galusmichal@outlook.com>
2025-02-08 18:10:19 +0100

    SCRUM-2 used @RequestMapping("/user") in UpdateUserController

--- src/main/java/pl/galushop/GaluShop/controller/user/UpdateUserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/user/UpdateUserController.java
@@ -1,9 +1,12 @@
 package pl.galushop.GaluShop.controller.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.UserRequest;
 import pl.galushop.GaluShop.entity.User;
@@ -11,24 +14,13 @@ import pl.galushop.GaluShop.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class UpdateUserController {
     private final UserService userService;
-    private final PasswordEncoder passwordEncoder;
 
-    @GetMapping("/updateUser")
-    public String updateUserFields(@RequestBody UserRequest userRequest){
-        if (userRequest.getUserId() == null) {
-            return "User ID is required";
-        }
-
-        User user = new User();
-        user.setFirstName(userRequest.getFirstName());
-        user.setLastName(userRequest.getLastName());
-        user.setEmail(userRequest.getEmail());
-        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
-
-        userService.updateUser(userRequest.getUserId(), user);
-
-        return "Success";
+    @PutMapping("/update")
+    public ResponseEntity<User> updateUserFields(@RequestBody UserRequest userRequest){
+        userService.updateUser(userRequest);
+        return ResponseEntity.ok(userService.getUser(userRequest.getUserId()));
     }
 }
