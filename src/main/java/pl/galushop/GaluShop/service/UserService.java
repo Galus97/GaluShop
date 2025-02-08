637c8c3e1e582f65818a3fbcebd9b72adda03756
michalgalus <galusmichal@outlook.com>
2025-02-08 18:09:25 +0100

    SCRUM-2 added save method in updateUser in UserService

--- src/main/java/pl/galushop/GaluShop/service/UserService.java
+++ src/main/java/pl/galushop/GaluShop/service/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.galushop.GaluShop.dto.UserRequest;
 import pl.galushop.GaluShop.entity.User;
 import pl.galushop.GaluShop.repository.UserRepository;
 
@@ -15,25 +16,27 @@ public class UserService {
     private final PasswordEncoder passwordEncoder;
 
     public User getUser(Long userId){
-        return userRepository.findByUserId(userId)
+        return userRepository.findById(userId)
                 .orElseThrow(() -> new UsernameNotFoundException("User with ID " + userId + " not found"));
     }
 
     public void deleteUser(Long userId){
-        User user = userRepository.findByUserId(userId)
+        User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UsernameNotFoundException("User with ID " + userId + " not found"));
         userRepository.delete(user);
     }
 
     @Transactional
-    public void updateUser(User user){
-        User existingUser = userRepository.findById(user.getUserId())
-                .orElseThrow(() -> new UsernameNotFoundException("User with ID " + user.getUserId() + " not found"));
+    public void updateUser(UserRequest userRequest){
+        User existingUser = userRepository.findById(userRequest.getUserId())
+                .orElseThrow(() -> new UsernameNotFoundException("User with ID " + userRequest.getUserId() + " not found"));
 
-        existingUser.setFirstName(user.getFirstName());
-        existingUser.setLastName(user.getLastName());
-        existingUser.setEmail(user.getEmail());
-        existingUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        existingUser.setFirstName(userRequest.getFirstName());
+        existingUser.setLastName(userRequest.getLastName());
+        existingUser.setEmail(userRequest.getEmail());
+        if(userRequest.getPassword() != null && !userRequest.getPassword().isEmpty()){
+            existingUser.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        }
 
         userRepository.save(existingUser);
     }
