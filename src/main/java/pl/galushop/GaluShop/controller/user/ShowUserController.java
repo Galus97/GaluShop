d07862df77bf845242be803cfa434c1c1c745845
michalgalus <galusmichal@outlook.com>
2025-02-08 18:11:40 +0100

    SCRUM-2 used @RequestMapping("/user") in ShowUserController

--- src/main/java/pl/galushop/GaluShop/controller/user/ShowUserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/user/ShowUserController.java
@@ -3,6 +3,7 @@ package pl.galushop.GaluShop.controller.user;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.UserRequest;
 import pl.galushop.GaluShop.entity.User;
@@ -10,10 +11,11 @@ import pl.galushop.GaluShop.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class ShowUserController {
     private final UserService userService;
 
-    @GetMapping("/user/show")
+    @GetMapping("/show")
     public User showUserInfo(@RequestBody UserRequest userRequest){
        return userService.getUser(userRequest.getUserId());
     }
