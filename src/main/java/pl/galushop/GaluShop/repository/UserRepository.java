e881f2092c9e8870595cf533552d53d66b5efa26
michalgalus <galusmichal@outlook.com>
2025-02-08 18:08:03 +0100

    SCRUM-2 deleted findByUserId from UserRepository

--- src/main/java/pl/galushop/GaluShop/repository/UserRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/UserRepository.java
@@ -1,17 +1,11 @@
 package pl.galushop.GaluShop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.transaction.annotation.Transactional;
 import pl.galushop.GaluShop.entity.User;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
     Optional<User> findByEmail(String email);
 
-    Optional<User> findByUserId(Long userId);
 }
