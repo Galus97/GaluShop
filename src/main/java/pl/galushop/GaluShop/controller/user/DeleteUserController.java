79d77c04d60049ca2b396683877a42276d8d1eaf
michalgalus <galusmichal@outlook.com>
2025-02-08 18:11:22 +0100

    SCRUM-2 used @RequestMapping("/user") in DeleteUserController

--- src/main/java/pl/galushop/GaluShop/controller/user/DeleteUserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/user/DeleteUserController.java
@@ -6,15 +6,17 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.UserRequest;
 import pl.galushop.GaluShop.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class DeleteUserController {
         private final UserService userService;
-    @DeleteMapping("/deleteUser/{id}")
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id){
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
