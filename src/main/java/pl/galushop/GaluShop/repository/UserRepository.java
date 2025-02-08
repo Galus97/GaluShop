df8e651170e5200a4d76a9597e3b671d85e3c1bd
michalgalus <galusmichal@outlook.com>
2025-02-08 17:50:02 +0100

    SCRUM-2 deleted updateUser in UserRepository

--- src/main/java/pl/galushop/GaluShop/repository/UserRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/UserRepository.java
@@ -2,7 +2,7 @@ package pl.galushop.GaluShop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 import pl.galushop.GaluShop.entity.User;
 
@@ -14,13 +14,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByUserId(Long userId);
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE User u SET u.firstName = :firstName, u.lastName = :lastName, u.email = :email, u.password = :password WHERE u.userId = :userId")
-    void updateUserByUserId(Long userId, String firstName, String lastName, String email, String password);
-
-    @Transactional
-    @Modifying
-    void deleteByUserId(Long userId);
 }
