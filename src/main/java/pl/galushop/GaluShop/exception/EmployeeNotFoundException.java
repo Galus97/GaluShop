c08e3850b835378b2b8418dff83b8e3d0ef65ba6
michalgalus <galusmichal@outlook.com>
2025-02-08 17:21:43 +0100

    SCRUM-2 used EmployeeNotFoundException class

---
+++ src/main/java/pl/galushop/GaluShop/exception/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package pl.galushop.GaluShop.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException (String message){
+        super(message);
+    }
+}
--- src/main/java/pl/galushop/GaluShop/exception/UserNotFoundException.java
+++ src/main/java/pl/galushop/GaluShop/exception/UserNotFoundException.java
@@ -1,11 +1,5 @@
 package pl.galushop.GaluShop.exception;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-
-@Getter
-@RequiredArgsConstructor
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException (String message){
         super(message);
