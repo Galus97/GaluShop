cc84de7e83503b27fc33b1ff4f662082a8617cd7
michalgalus <galusmichal@outlook.com>
2025-02-09 13:02:29 +0100

    SCRUM-2 added RequestMapping to DeleteEmployeeController

--- src/main/java/pl/galushop/GaluShop/controller/employee/UpdateEmployeeController.java
+++ src/main/java/pl/galushop/GaluShop/controller/employee/UpdateEmployeeController.java
@@ -4,17 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.EmployeeRequest;
 import pl.galushop.GaluShop.service.EmployeeService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/employee")
 public class UpdateEmployeeController {
     private final EmployeeService employeeService;
     private final PasswordEncoder passwordEncoder;
 
-    @GetMapping("/updateEmployee")
+    @GetMapping("/update")
     private String updateEmployee(@RequestBody EmployeeRequest employeeRequest){
         employeeService.updateEmployee(
                 employeeRequest.getEmployeeId(),
