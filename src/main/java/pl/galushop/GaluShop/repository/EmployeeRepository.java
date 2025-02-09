c5de9e9fcec34852d46f830d5a6d63105e914703
michalgalus <galusmichal@outlook.com>
2025-02-09 13:27:25 +0100

    SCRUM-2 deleted updateEmployeeByEmployeeId in EmployeeRepository

--- src/main/java/pl/galushop/GaluShop/repository/EmployeeRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/EmployeeRepository.java
@@ -1,19 +1,10 @@
 package pl.galushop.GaluShop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 import pl.galushop.GaluShop.entity.Employee;
 
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Optional<Employee> findByEmail(String email);
-
-    @Transactional
-    @Modifying
-    @Query("UPDATE Employee e SET e.firstName = : firstName, e.lastName = :lastName, " +
-            "e.email = :email, e.password = :password WHERE e.employeeId = :employeeId")
-    void updateEmployeeByEmployeeId(Long employeeId, String firstName, String lastName, String email, String password);
 }
