fb5d937fb1be98d685b04c20d8bb7660a2dfed6e
michalgalus <galusmichal@outlook.com>
2025-02-11 16:03:48 +0100

    SCRUM-2 added general RequestMapping & PutMapping

--- src/main/java/pl/galushop/GaluShop/controller/userData/SaveUserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/userData/SaveUserDataController.java
@@ -1,36 +1,23 @@
 package pl.galushop.GaluShop.controller.userData;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.UserDataRequest;
-import pl.galushop.GaluShop.entity.User;
-import pl.galushop.GaluShop.entity.UserData;
 import pl.galushop.GaluShop.service.UserDataService;
-import pl.galushop.GaluShop.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/userData")
 public class SaveUserDataController {
-    private final UserService userService;
     private final UserDataService userDataService;
 
-    @PostMapping("/userData")
+    @PutMapping("/save")
     public String saveUserData(@RequestBody UserDataRequest userDataRequest){
 
-        User user = userService.getUser(userDataRequest.getUserId());
-
-        UserData userData = new UserData();
-        userData.setUser(user);
-        userData.setCity(userDataRequest.getCity());
-        userData.setStreet(userDataRequest.getStreet());
-        userData.setStreetNumber(userDataRequest.getStreetNumber());
-        userData.setApartmentNumber(userDataRequest.getApartmentNumber());
-        userData.setZipCode(userDataRequest.getZipCode());
-        userData.setPhoneNumber(userDataRequest.getPhoneNumber());
-
-        userDataService.saveUserDataToDatabase(userData);
+        userDataService.saveUserData(userDataRequest);
 
         return "success";
     }
