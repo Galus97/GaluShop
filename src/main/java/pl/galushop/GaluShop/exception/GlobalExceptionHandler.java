64b7aaa3ed4b6d343636a566a3bc36dda47ac96a
michalgalus <galusmichal@outlook.com>
2025-02-11 15:05:44 +0100

    SCRUM-2 added handleWarehouseProductNotFoundException to GlobalExceptionHandler

--- src/main/java/pl/galushop/GaluShop/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/galushop/GaluShop/exception/GlobalExceptionHandler.java
@@ -25,6 +25,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
+    @ExceptionHandler(WarehouseProductNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleWarehouseProductNotFoundException(WarehouseProductNotFoundException e){
+        Map<String, String> response = new HashMap<>();
+        response.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
