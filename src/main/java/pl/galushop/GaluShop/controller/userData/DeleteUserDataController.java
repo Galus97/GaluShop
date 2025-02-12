dcf1d98d8844a39aed1128bf17bdc3e3c718f797
michalgalus <galusmichal@outlook.com>
2025-02-12 08:22:58 +0100

    SCRUM-2 added RequestMapping & DeleteMapping in DeleteUserDataController

--- src/main/java/pl/galushop/GaluShop/controller/userData/DeleteUserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/userData/DeleteUserDataController.java
@@ -1,18 +1,21 @@
 package pl.galushop.GaluShop.controller.userData;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.UserDataRequest;
 import pl.galushop.GaluShop.service.UserDataService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/userData")
 public class DeleteUserDataController {
     private final UserDataService userDataService;
 
-    @GetMapping("/userData/delete")
+    @DeleteMapping("/delete")
     public String deleteUserData(@RequestBody UserDataRequest userDataRequest){
         userDataService.deleteUserData(userDataRequest.getUserDataId());
         return "Success";
