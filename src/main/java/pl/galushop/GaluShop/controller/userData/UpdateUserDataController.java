01b44f4184a955483ba885c09f236797f65b0f5e
michalgalus <galusmichal@outlook.com>
2025-02-12 08:14:20 +0100

    SCRUM-2 added RequestMapping & PutMapping to UpdateUserDataController

--- src/main/java/pl/galushop/GaluShop/controller/userData/UpdateUserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/userData/UpdateUserDataController.java
@@ -2,7 +2,9 @@ package pl.galushop.GaluShop.controller.userData;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.UserDataRequest;
 import pl.galushop.GaluShop.entity.UserData;
@@ -10,20 +12,13 @@ import pl.galushop.GaluShop.service.UserDataService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/userData")
 public class UpdateUserDataController {
     private final UserDataService userDataService;
 
-    @GetMapping("/userData/update")
+    @PutMapping("/update")
     public UserData updateUserData(@RequestBody UserDataRequest userDataRequest){
-        userDataService.updateUserData(
-                userDataRequest.getUserDataId(),
-                userDataRequest.getCity(),
-                userDataRequest.getStreet(),
-                userDataRequest.getStreetNumber(),
-                userDataRequest.getApartmentNumber(),
-                userDataRequest.getZipCode(),
-                userDataRequest.getPhoneNumber()
-        );
+        userDataService.updateUserData(userDataRequest);
 
         return userDataService.showUserData(userDataRequest.getUserDataId());
     }
