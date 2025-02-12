24379f768856fe07eaf3ce7e767b17cd794a6e8a
michalgalus <galusmichal@outlook.com>
2025-02-12 08:05:26 +0100

    SCRUM-2 added ResponseEntity & to PathVariable to ShowUserDataController

--- src/main/java/pl/galushop/GaluShop/controller/userData/ShowUserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/userData/ShowUserDataController.java
@@ -1,7 +1,9 @@
 package pl.galushop.GaluShop.controller.userData;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.entity.UserData;
@@ -13,8 +15,8 @@ import pl.galushop.GaluShop.service.UserDataService;
 public class ShowUserDataController {
     private final UserDataService userDataService;
 
-    @GetMapping("/show")
-    public UserData showUserData(){
-        return userDataService.showUserDataByUserId(1L);
+    @GetMapping("/show/{id}")
+    public ResponseEntity<UserData> showUserData(@PathVariable Long id){
+        return ResponseEntity.ok(userDataService.showUserDataByUserId(id));
     }
 }
