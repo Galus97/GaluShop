89eccf0ac361a4fe3d66eb44f993322b821ed3b4
michalgalus <galusmichal@outlook.com>
2025-02-12 08:03:24 +0100

    SCRUM-2 added RequestMapping to ShowUserDataController

--- src/main/java/pl/galushop/GaluShop/controller/userData/ShowUserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/userData/ShowUserDataController.java
@@ -2,16 +2,18 @@ package pl.galushop.GaluShop.controller.userData;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.entity.UserData;
 import pl.galushop.GaluShop.service.UserDataService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/userData")
 public class ShowUserDataController {
     private final UserDataService userDataService;
 
-    @GetMapping("/userData/show")
+    @GetMapping("/show")
     public UserData showUserData(){
         return userDataService.showUserDataByUserId(1L);
     }
