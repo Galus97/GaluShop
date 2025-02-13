6ddfcfd9d342a9ded84d899e44eb2d7f62f783ed
michalgalus <galusmichal@outlook.com>
2025-02-13 14:03:07 +0100

    SCRUM-2 added ResponseEntity to SaveProductController

--- src/main/java/pl/galushop/GaluShop/controller/product/SaveProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/product/SaveProductController.java
@@ -1,28 +1,26 @@
 package pl.galushop.GaluShop.controller.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.galushop.GaluShop.dto.ProductImageRequest;
 import pl.galushop.GaluShop.dto.ProductRequest;
 import pl.galushop.GaluShop.entity.Product;
-import pl.galushop.GaluShop.entity.ProductImages;
-import pl.galushop.GaluShop.service.ProductImagesService;
 import pl.galushop.GaluShop.service.ProductService;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class ProductAddController {
+@RequestMapping("/Product")
+public class SaveProductController {
 
     private final ProductService productService;
 
-    @GetMapping("/add")
-    public String addNewProduct(@RequestBody ProductRequest productRequest) {
+    @PutMapping("/add")
+    public ResponseEntity<Product> addNewProduct(@RequestBody ProductRequest productRequest) {
         productService.saveProductToDatabase(productRequest);
-        return "success";
+        return ResponseEntity.ok(productService.findProductById(productRequest.getProductId()));
     }
 }
