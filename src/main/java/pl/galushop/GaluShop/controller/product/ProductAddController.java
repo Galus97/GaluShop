feddbd44d9561dc965017869ec5da2ed17023728
michalgalus <galusmichal@outlook.com>
2025-02-13 13:59:27 +0100

    SCRUM-2 changed method to not created Product Object in ProductAddController

--- src/main/java/pl/galushop/GaluShop/controller/product/ProductAddController.java
+++ src/main/java/pl/galushop/GaluShop/controller/product/ProductAddController.java
@@ -3,6 +3,7 @@ package pl.galushop.GaluShop.controller.product;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.ProductImageRequest;
 import pl.galushop.GaluShop.dto.ProductRequest;
@@ -18,26 +19,10 @@ import java.util.List;
 public class ProductAddController {
 
     private final ProductService productService;
-    private final ProductImagesService productImagesService;
 
-    @GetMapping("/addProduct")
+    @GetMapping("/add")
     public String addNewProduct(@RequestBody ProductRequest productRequest) {
-        Product product = new Product();
-        product.setProductName(productRequest.getProductName());
-        product.setCategory(productRequest.getCategory());
-        product.setPrice(productRequest.getPrice());
-        product.setCategoryId(productRequest.getCategoryId());
-        product.setDescription(productRequest.getDescription());
-        productService.saveProductToDatabase(product);
-
-        List<ProductImageRequest> productImagesList = productRequest.getProductImages();
-        for (ProductImageRequest productImage : productImagesList) {
-            ProductImages productImages = new ProductImages();
-            productImages.setProduct(product);
-            productImages.setImgSrc(productImage.getImgSrc());
-            productImages.setAltImg(productImage.getAltImg());
-            productImagesService.saveProductImagesToDatabase(productImages);
-        }
+        productService.saveProductToDatabase(productRequest);
         return "success";
     }
 }
