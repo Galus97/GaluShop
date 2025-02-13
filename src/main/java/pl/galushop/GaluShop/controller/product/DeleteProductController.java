cef11b2e310b6e9d94220a7b737d63993848d6b5
michalgalus <galusmichal@outlook.com>
2025-02-13 15:50:05 +0100

    SCRUM-2 changed name to DeleteProductController

--- src/main/java/pl/galushop/GaluShop/controller/product/DeleteProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/product/DeleteProductController.java
@@ -1,21 +1,17 @@
 package pl.galushop.GaluShop.controller.product;
 
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.galushop.GaluShop.dto.ProductRequest;
 import pl.galushop.GaluShop.service.ProductService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/product")
-public class ProductDeleteController {
+public class DeleteProductController {
     private final ProductService productService;
 
     @DeleteMapping("/delete/{id}")
