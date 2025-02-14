7870627f849202c408137eb4856a1a7316a7fea2
michalgalus <galusmichal@outlook.com>
2025-02-14 13:51:37 +0100

    SCRUM-2 added RequestMapping & PathVariable to WarehouseProductInfoController

--- src/main/java/pl/galushop/GaluShop/controller/warehouse/WarehouseProductInfoController.java
+++ src/main/java/pl/galushop/GaluShop/controller/warehouse/WarehouseProductInfoController.java
@@ -2,7 +2,9 @@ package pl.galushop.GaluShop.controller.warehouse;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.WarehouseProductRequest;
 import pl.galushop.GaluShop.entity.WarehouseProduct;
@@ -10,11 +12,12 @@ import pl.galushop.GaluShop.service.WarehouseProductService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/warehouseProduct")
 public class WarehouseProductInfoController {
     private final WarehouseProductService warehouseProductService;
 
-    @GetMapping("/showInfoWarehouseProduct")
-    public WarehouseProduct showInfoWarehouseProduct(@RequestBody WarehouseProductRequest warehouseProductRequest){
-        return warehouseProductService.getWarehouseProduct(warehouseProductRequest.getProductId());
+    @GetMapping("/show/{id}")
+    public WarehouseProduct showInfoWarehouseProduct(@PathVariable Long id){
+        return warehouseProductService.getWarehouseProduct(id);
     }
 }
