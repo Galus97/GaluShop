8e0577bf1d90d297a6abbf785d9e53961cc10f5d
michalgalus <galusmichal@outlook.com>
2025-02-14 14:04:01 +0100

    SCRUM-2 added RequestMapping & DeleteMapping to DeleteWarehouseProductController

--- src/main/java/pl/galushop/GaluShop/controller/warehouse/DeleteWarehouseProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/warehouse/DeleteWarehouseProductController.java
@@ -1,18 +1,21 @@
 package pl.galushop.GaluShop.controller.warehouse;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.WarehouseProductRequest;
 import pl.galushop.GaluShop.service.WarehouseProductService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/warehouseProduct")
 public class DeleteWarehouseProductController {
     private final WarehouseProductService warehouseProductService;
 
-    @GetMapping("/deleteWarehouseProduct")
+    @DeleteMapping("/delete/{id}")
     public String deleteWarehouseProduct(@RequestBody WarehouseProductRequest warehouseProductRequest){
         Long warehouseProductId = warehouseProductRequest.getWarehouseProductId();
         if(warehouseProductId != null){
