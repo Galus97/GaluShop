83222aa3452fd7d29d767a269736955380f57281
michalgalus <galusmichal@outlook.com>
2025-02-14 14:16:49 +0100

    SCRUM-2 added RequestMapping & PutMapping to AddOrderController

--- src/main/java/pl/galushop/GaluShop/controller/order/AddOrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/order/AddOrderController.java
@@ -1,37 +1,22 @@
 package pl.galushop.GaluShop.controller.order;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.OrderRequest;
-import pl.galushop.GaluShop.entity.Order;
-import pl.galushop.GaluShop.entity.Product;
 import pl.galushop.GaluShop.service.OrderService;
-import pl.galushop.GaluShop.service.ProductService;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/order/")
 public class AddOrderController {
     private final OrderService orderService;
-    private final ProductService productService;
 
-    @PostMapping("/addOrder")
+    @PutMapping("/save")
     public String saveNewOrder(@RequestBody OrderRequest orderRequest) {
-        List<Product> products = orderRequest.getProductIds().stream()
-                .map(productService::getProductById)
-                .collect(Collectors.toList());
-
-        Order order = new Order();
-        order.setLocalDateTime(orderRequest.getLocalDateTime());
-        order.setStatus(orderRequest.getOrderStatus());
-        order.setUser(order.getUser());
-        order.setProducts(products);
-
-        orderService.saveOrderToDatabase(order);
+        orderService.saveOrderToDatabase(orderRequest);
         return "Order added successfully!";
     }
 }
