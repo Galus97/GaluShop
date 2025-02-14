af59c3d3558703976b1460f0c3c32490246eeb1e
michalgalus <galusmichal@outlook.com>
2025-02-14 13:59:30 +0100

    SCRUM-2 added RequestMapping & PutMapping to UpdateQuantityWarehouseController

--- src/main/java/pl/galushop/GaluShop/controller/warehouse/UpdateQuantityWarehouseController.java
+++ src/main/java/pl/galushop/GaluShop/controller/warehouse/UpdateQuantityWarehouseController.java
@@ -1,17 +1,19 @@
 package pl.galushop.GaluShop.controller.warehouse;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.WarehouseProductRequest;
 import pl.galushop.GaluShop.service.WarehouseProductService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/warehouse")
 public class UpdateQuantityWarehouseController {
     private final WarehouseProductService warehouseProductService;
-    @GetMapping("/updateQuantity")
+    @PutMapping("/update")
     public String updateQuantity(@RequestBody WarehouseProductRequest warehouseProductRequest){
         warehouseProductService.updateQuantityByProductId(warehouseProductRequest.getProductId(),
                 warehouseProductRequest.getQuantity());
