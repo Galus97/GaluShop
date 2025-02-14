5c52cabeca25d04b5cf3024cb9ff4041e5b6a74c
michalgalus <galusmichal@outlook.com>
2025-02-14 13:57:43 +0100

    SCRUM-2 added ResponseEntity to SaveWarehouseProductController

--- src/main/java/pl/galushop/GaluShop/controller/warehouse/SaveWarehouseProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/warehouse/SaveWarehouseProductController.java
@@ -1,11 +1,13 @@
 package pl.galushop.GaluShop.controller.warehouse;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.WarehouseProductRequest;
+import pl.galushop.GaluShop.entity.WarehouseProduct;
 import pl.galushop.GaluShop.service.WarehouseProductService;
 
 @RestController
@@ -14,9 +16,9 @@ import pl.galushop.GaluShop.service.WarehouseProductService;
 public class SaveWarehouseProductController {
     private final WarehouseProductService warehouseProductService;
 
-    @PutMapping("/addProduct")
-    public String addProductToWarehouse(@RequestBody WarehouseProductRequest warehouseProductRequest) {
+    @PutMapping("/save")
+    public ResponseEntity<WarehouseProduct> addProductToWarehouse(@RequestBody WarehouseProductRequest warehouseProductRequest) {
         warehouseProductService.addProductToWarehouse(warehouseProductRequest);
-        return "success";
+        return ResponseEntity.ok(warehouseProductService.getWarehouseProduct(warehouseProductRequest.getProductId()));
     }
 }
