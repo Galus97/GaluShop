d23da2b87c0a6830ae54f649b7338f02743d376a
michalgalus <galusmichal@outlook.com>
2025-02-15 13:38:22 +0100

    SCRUM-2 added RequestMapping & DeleteMapping to DeleteOrderController

--- src/main/java/pl/galushop/GaluShop/controller/order/DeleteOrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/order/DeleteOrderController.java
@@ -1,18 +1,21 @@
 package pl.galushop.GaluShop.controller.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.OrderRequest;
 import pl.galushop.GaluShop.service.OrderService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/order")
 public class DeleteOrderController {
     private final OrderService orderService;
 
-    @GetMapping("/deleteOrder")
+    @DeleteMapping("/delete")
     public String deleteOrder(@RequestBody OrderRequest orderRequest){
         orderService.deleteOrder(orderRequest.getUserId());
         return "Success";
