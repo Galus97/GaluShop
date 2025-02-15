cf09e197dd279d7677e8940b174b53c2070a9b6d
michalgalus <galusmichal@outlook.com>
2025-02-15 13:42:07 +0100

    SCRUM-2 added PathVariable & RequestMapping to  SpecificOrderController

--- src/main/java/pl/galushop/GaluShop/controller/order/SpecificOrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/order/SpecificOrderController.java
@@ -2,18 +2,20 @@ package pl.galushop.GaluShop.controller.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.entity.Order;
 import pl.galushop.GaluShop.service.OrderService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/order")
 public class SpecificOrderController {
     private final OrderService orderService;
 
-
-    @GetMapping("/specificOrder")
-    public Order showSpecificOrder(){
+    @GetMapping("/show/{id}")
+    public Order showSpecificOrder(@PathVariable Long id){
         return orderService.getSpecificOrder(1L);
     }
 }
