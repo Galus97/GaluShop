c1b19a7306c00f6483e4b9057f1645340e5c701a
michalgalus <galusmichal@outlook.com>
2025-02-15 13:37:21 +0100

    SCRUM-2 added ResponseEntity to UpdateOrderController

--- src/main/java/pl/galushop/GaluShop/controller/order/UpdateOrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/order/UpdateOrderController.java
@@ -1,18 +1,14 @@
 package pl.galushop.GaluShop.controller.order;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.OrderRequest;
-import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.entity.Order;
 import pl.galushop.GaluShop.service.OrderService;
-import pl.galushop.GaluShop.service.ProductService;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +17,9 @@ public class UpdateOrderController {
     private final OrderService orderService;
 
     @PutMapping("/update")
-    public String updateOrder(@RequestBody OrderRequest orderRequest){
-    orderService.updateOrder(orderRequest);
-        return "Success";
+    public ResponseEntity<Order> updateOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.updateOrder(orderRequest);
+        return ResponseEntity.ok(orderService.getOrder(orderRequest.getOrderId()));
     }
 
 }
