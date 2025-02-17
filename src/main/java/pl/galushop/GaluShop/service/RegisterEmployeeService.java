2ef62159ca0505902cf7826a1b45baf4b9702004
michalgalus <galusmichal@outlook.com>
2025-02-17 10:00:39 +0100

    SCRUM-2 added save logic to RegisterEmployeeService

--- src/main/java/pl/galushop/GaluShop/service/RegisterEmployeeService.java
+++ src/main/java/pl/galushop/GaluShop/service/RegisterEmployeeService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.galushop.GaluShop.component.RegisterValidator;
+import pl.galushop.GaluShop.dto.EmployeeRequest;
 import pl.galushop.GaluShop.entity.Employee;
 import pl.galushop.GaluShop.exception.ValidationException;
 import pl.galushop.GaluShop.repository.EmployeeRepository;
@@ -16,13 +17,21 @@ public class RegisterEmployeeService {
     private final EmployeeRepository employeeRepository;
     private final PasswordEncoder passwordEncoder;
     private final RegisterValidator registerValidator;
+    private final EmailService emailService;
 
-    public void saveNewEmployee(Employee employee) throws ValidationException {
-        List<String> validationFailures = registerValidator.validateUserErrors(employee);
-        if (validationFailures.isEmpty()) {
-            //employee.setEmployeeId(null);
+    //Dokończyć
+    public void saveEmployee(EmployeeRequest employeeRequest) throws ValidationException{
+        Employee employee = new Employee();
+        employee.setFirstName(employeeRequest.getFirstName());
+        employee.setLastName(employeeRequest.getLastName());
+        employee.setEmail(employeeRequest.getEmail());
+
+        List<String> validationFailures = registerValidator.validateErrors(employee);
+        if(validationFailures.isEmpty()){
+            employee.setEmailCode(emailService.emailCodeValue());
             employee.setPassword(passwordEncoder.encode(employee.getPassword()));
             employeeRepository.save(employee);
+            emailService.sendEmail(employeeRequest.getEmail());
         } else {
             throw new ValidationException(validationFailures);
         }
