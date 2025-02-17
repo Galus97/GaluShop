4b47b83665e2f276e954d3fbc7054cb207d928dd
michalgalus <galusmichal@outlook.com>
2025-02-17 10:04:29 +0100

    SCRUM-2 added ResponseEntity to RegisterEmployeeController

--- src/main/java/pl/galushop/GaluShop/controller/employee/RegisterEmployeeController.java
+++ src/main/java/pl/galushop/GaluShop/controller/employee/RegisterEmployeeController.java
@@ -1,24 +1,31 @@
 package pl.galushop.GaluShop.controller.employee;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.EmployeeRequest;
+import pl.galushop.GaluShop.entity.Employee;
 import pl.galushop.GaluShop.exception.ValidationException;
+import pl.galushop.GaluShop.service.EmployeeService;
 import pl.galushop.GaluShop.service.RegisterEmployeeService;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/employee")
 public class RegisterEmployeeController {
     private final RegisterEmployeeService registerEmployeeService;
+    private final EmployeeService employeeService;
 
-    @PostMapping("/registerEmployee")
-    public String saveNewEmployee(@RequestBody EmployeeRequest employeeRequest) {
+    @PostMapping("/register")
+    public ResponseEntity<Employee> saveNewEmployee(@RequestBody EmployeeRequest employeeRequest) {
         try {
             registerEmployeeService.saveEmployee(employeeRequest);
+            return ResponseEntity.ok(employeeService.getEmployee(employeeRequest.getEmployeeId()));
         } catch (ValidationException e) {
-            throw new RuntimeException(e);
+            return ResponseEntity.noContent().build();
         }
     }
 }
