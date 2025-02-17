3455f8db51482bc13d22762b50c28dd798b897c6
michalgalus <galusmichal@outlook.com>
2025-02-17 10:01:19 +0100

    SCRUM-2 deleted save logic from RegisterEmployeeController

--- src/main/java/pl/galushop/GaluShop/controller/employee/RegisterEmployeeController.java
+++ src/main/java/pl/galushop/GaluShop/controller/employee/RegisterEmployeeController.java
@@ -2,45 +2,23 @@ package pl.galushop.GaluShop.controller.employee;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import pl.galushop.GaluShop.dto.EmployeeRequest;
-import pl.galushop.GaluShop.entity.Employee;
 import pl.galushop.GaluShop.exception.ValidationException;
-import pl.galushop.GaluShop.service.EmailService;
 import pl.galushop.GaluShop.service.RegisterEmployeeService;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class RegisterEmployeeController {
     private final RegisterEmployeeService registerEmployeeService;
-    private final EmailService emailService;
 
     @PostMapping("/registerEmployee")
-    public String saveNewEmployee(@RequestBody EmployeeRequest employeeRequest){
-
-        Employee employee = new Employee();
-        employee.setFirstName(employeeRequest.getFirstName());
-        employee.setLastName(employeeRequest.getLastName());
-        employee.setEmail(employeeRequest.getEmail());
-        employee.setPassword(employeeRequest.getPassword());
-        employee.setEmailCode(emailService.emailCodeValue());
-
+    public String saveNewEmployee(@RequestBody EmployeeRequest employeeRequest) {
         try {
-            registerEmployeeService.saveNewEmployee(employee);
-            emailService.sendEmail(employeeRequest.getEmail());
-        } catch (ValidationException exception) {
-            List<String> validationErrors = exception.getValidationErrors();
-            for (String error : validationErrors) {
-                System.out.println(error);
-            }
-            return "faild";
+            registerEmployeeService.saveEmployee(employeeRequest);
+        } catch (ValidationException e) {
+            throw new RuntimeException(e);
         }
-
-        return "succes";
     }
 }
