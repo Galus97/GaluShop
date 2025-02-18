7aa398fb73f26bd288f050f3582482c45d24c405
michalgalus <galusmichal@outlook.com>
2025-02-18 22:55:42 +0100

    SCRUM-2 added deleteEmployee method to EmployeeController

--- src/main/java/pl/galushop/GaluShop/controller/employee/EmployeeController.java
+++ src/main/java/pl/galushop/GaluShop/controller/employee/EmployeeController.java
@@ -1,9 +1,22 @@
 package pl.galushop.GaluShop.controller.employee;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.service.EmployeeService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/employee")
 public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable Long id){
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
 }
