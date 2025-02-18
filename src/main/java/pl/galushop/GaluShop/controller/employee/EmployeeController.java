a8333e32915201325075ae49f223e087647942f1
michalgalus <galusmichal@outlook.com>
2025-02-18 22:56:39 +0100

    SCRUM-2 added showEmployee method to EmployeeController

--- src/main/java/pl/galushop/GaluShop/controller/employee/EmployeeController.java
+++ src/main/java/pl/galushop/GaluShop/controller/employee/EmployeeController.java
@@ -3,9 +3,11 @@ package pl.galushop.GaluShop.controller.employee;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.Employee;
 import pl.galushop.GaluShop.service.EmployeeService;
 
 @RestController
@@ -13,6 +15,11 @@ import pl.galushop.GaluShop.service.EmployeeService;
 @RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> showEmployee(@PathVariable Long id){
+        return ResponseEntity.ok(employeeService.getEmployee(id));
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable Long id){
