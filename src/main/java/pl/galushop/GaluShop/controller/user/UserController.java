2425f8494d3937f317f013ba687277d8a38249ca
michalgalus <galusmichal@outlook.com>
2025-02-19 13:40:09 +0100

    SCRUM-2 added updateUser to UserController

--- src/main/java/pl/galushop/GaluShop/controller/user/UserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +36,12 @@ public class UserController {
         } catch (ValidationException e) {
             return ResponseEntity.noContent().build();
         }
+    }
+
+    @PutMapping
+    public ResponseEntity<User> updateUser(@RequestBody UserRequest userRequest){
+        userService.updateUser(userRequest);
+        return ResponseEntity.ok(userService.getUser(userRequest.getUserId()));
     }
 
     @DeleteMapping("/{id}")
