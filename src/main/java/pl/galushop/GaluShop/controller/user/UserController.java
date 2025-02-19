842f85a4226651295e2646f182474e030d3ed7a3
michalgalus <galusmichal@outlook.com>
2025-02-19 13:36:44 +0100

    SCRUM-2 added UserController.java

---
+++ src/main/java/pl/galushop/GaluShop/controller/user/UserController.java
@@ -0,0 +1,11 @@
+package pl.galushop.GaluShop.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+}
