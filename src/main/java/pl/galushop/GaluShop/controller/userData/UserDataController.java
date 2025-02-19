dcafdda095543ded6ae2ee21c200a74720bd4261
michalgalus <galusmichal@outlook.com>
2025-02-19 13:32:19 +0100

    SCRUM-2 added deleteUserData to UserDataController

--- src/main/java/pl/galushop/GaluShop/controller/userData/UserDataController.java
+++ src/main/java/pl/galushop/GaluShop/controller/userData/UserDataController.java
@@ -1,12 +1,22 @@
 package pl.galushop.GaluShop.controller.userData;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.service.UserDataService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/userData")
 public class UserDataController {
+    private final UserDataService userDataService;
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUserData(@PathVariable Long id){
+        userDataService.deleteUserData(id);
+        return ResponseEntity.noContent().build();
+    }
 }
