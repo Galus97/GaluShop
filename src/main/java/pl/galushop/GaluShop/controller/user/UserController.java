a496a4e192a2209b4ac65b4a31b2a6d0fb261db7
michalgalus <galusmichal@outlook.com>
2025-02-19 13:37:20 +0100

    SCRUM-2 added deleteUser to UserController

--- src/main/java/pl/galushop/GaluShop/controller/user/UserController.java
+++ src/main/java/pl/galushop/GaluShop/controller/user/UserController.java
@@ -1,11 +1,22 @@
 package pl.galushop.GaluShop.controller.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
+    private final UserService userService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
+    }
 }
