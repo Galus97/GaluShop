18d4c3ff71270750e3060c16e7bfa2fc8e23189c
michalgalus <galusmichal@outlook.com>
2025-02-20 22:57:41 +0100

    SCRUM-2 added showProduct to ProductController

--- src/main/java/pl/galushop/GaluShop/controller/product/ProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/product/ProductController.java
@@ -1,4 +1,22 @@
 package pl.galushop.GaluShop.controller.product;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.service.ProductService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/product")
 public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> showProduct(@PathVariable Long id){
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
 }
