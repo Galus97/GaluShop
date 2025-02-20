c33d21f57832b7d0c3e90d4f69a2605e2737fcd2
michalgalus <galusmichal@outlook.com>
2025-02-20 23:07:02 +0100

    SCRUM-2 added deleteOrder to OrderController

--- src/main/java/pl/galushop/GaluShop/controller/order/OrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/order/OrderController.java
@@ -2,9 +2,11 @@ package pl.galushop.GaluShop.controller.order;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +29,17 @@ public class OrderController {
     public ResponseEntity<Order> saveOrder(@RequestBody OrderRequest orderRequest) {
         orderService.saveOrderToDatabase(orderRequest);
         return ResponseEntity.ok(orderService.getOrder(orderRequest.getOrderId()));
+    }
+
+    @PutMapping
+    public ResponseEntity<Order> updateOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.updateOrder(orderRequest);
+        return ResponseEntity.ok(orderService.getOrder(orderRequest.getOrderId()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long id){
+        orderService.deleteOrder(id);
+        return ResponseEntity.noContent().build();
     }
 }
