cfcaec05071db7497aac75c6f9fa232da08ed385
michalgalus <galusmichal@outlook.com>
2025-02-20 23:04:59 +0100

    SCRUM-2 added showOrder to OrderController

--- src/main/java/pl/galushop/GaluShop/controller/order/OrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/order/OrderController.java
@@ -1,11 +1,22 @@
 package pl.galushop.GaluShop.controller.order;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.Order;
+import pl.galushop.GaluShop.service.OrderService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/order")
 public class OrderController {
+    private final OrderService orderService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> showOrder(@PathVariable Long id){
+        return ResponseEntity.ok(orderService.getOrder(id));
+    }
 }
