28eb51bae426a17a055664099c8b7643fb2961c8
michalgalus <galusmichal@outlook.com>
2025-02-22 15:01:34 +0100

    SCRUM-2 added fields to OrderProduct

--- src/main/java/pl/galushop/GaluShop/entity/OrderProduct.java
+++ src/main/java/pl/galushop/GaluShop/entity/OrderProduct.java
@@ -1,6 +1,10 @@
 package pl.galushop.GaluShop.entity;
 
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,4 +18,20 @@ import lombok.Setter;
 @AllArgsConstructor
 @Table(name = "orders_product")
 public class OrderProduct {
+
+    @EmbeddedId
+    private OrderProductId id = new OrderProductId();
+
+    @ManyToOne
+    @MapsId("orderId")
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @MapsId("productId")
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private Integer quantity;
+
 }
