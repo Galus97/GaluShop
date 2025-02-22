e78531a9b8dc337b1c66fd7a052676c7b033e9f9
michalgalus <galusmichal@outlook.com>
2025-02-22 15:08:03 +0100

    SCRUM-2 added findByOrderOrderId to OrderProductRepository

--- src/main/java/pl/galushop/GaluShop/repository/OrderProductRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/OrderProductRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.galushop.GaluShop.entity.OrderProduct;
 import pl.galushop.GaluShop.entity.OrderProductId;
 
+import java.util.List;
+
 public interface OrderProductRepository extends JpaRepository<OrderProduct, OrderProductId> {
+    List<OrderProduct> findByOrderOrderId(Long orderId);
 }
