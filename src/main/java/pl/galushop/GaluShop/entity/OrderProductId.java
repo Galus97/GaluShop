091917b845f3050bcf53c92529cf1548a16c3542
michalgalus <galusmichal@outlook.com>
2025-02-22 14:57:48 +0100

    SCRUM-2 added fields & annotation to  OrderProductId

--- src/main/java/pl/galushop/GaluShop/entity/OrderProductId.java
+++ src/main/java/pl/galushop/GaluShop/entity/OrderProductId.java
@@ -1,6 +1,21 @@
 package pl.galushop.GaluShop.entity;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class OrderProductId implements Serializable {
+    private Long orderId;
+    private Long productId;
 }
