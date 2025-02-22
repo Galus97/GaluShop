19de814639220081a8ffd0b575fcbe1bf005a9fe
michalgalus <galusmichal@outlook.com>
2025-02-22 14:54:53 +0100

    SCRUM-2 added OrderProduct.java

---
+++ src/main/java/pl/galushop/GaluShop/entity/OrderProduct.java
@@ -0,0 +1,17 @@
+package pl.galushop.GaluShop.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders_product")
+public class OrderProduct {
+}
