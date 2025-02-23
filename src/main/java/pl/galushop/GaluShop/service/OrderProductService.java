4aa8f2a056931f1c26d75e24c62abefe7e801938
michalgalus <galusmichal@outlook.com>
2025-02-23 10:58:26 +0100

    SCRUM-2 added OrderProductService.class

---
+++ src/main/java/pl/galushop/GaluShop/service/OrderProductService.java
@@ -0,0 +1,9 @@
+package pl.galushop.GaluShop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderProductService {
+}
