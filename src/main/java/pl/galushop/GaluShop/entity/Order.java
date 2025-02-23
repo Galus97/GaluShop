0c2c251b505d1e9240e0517a1b6a50baa9a5438d
michalgalus <galusmichal@outlook.com>
2025-02-23 10:39:24 +0100

    SCRUM-2 added orderProducts instead of Product in Order entity

--- src/main/java/pl/galushop/GaluShop/entity/Order.java
+++ src/main/java/pl/galushop/GaluShop/entity/Order.java
@@ -1,11 +1,12 @@
 package pl.galushop.GaluShop.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -36,6 +37,6 @@ public class Order {
     @ManyToOne
     private User user;
 
-    @ManyToMany
-    private List<Product> products;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderProduct> orderProducts;
 }
