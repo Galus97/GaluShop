4f8de057eaf29b9613f8898c6569332aca016fde
michalgalus <galusmichal@outlook.com>
2025-02-23 11:05:45 +0100

    SCRUM-2 added saveOrderProduct to OrderProductService

--- src/main/java/pl/galushop/GaluShop/service/OrderProductService.java
+++ src/main/java/pl/galushop/GaluShop/service/OrderProductService.java
@@ -2,8 +2,26 @@ package pl.galushop.GaluShop.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.galushop.GaluShop.entity.Order;
+import pl.galushop.GaluShop.entity.OrderProduct;
+import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.exception.OrderNotFoundException;
+import pl.galushop.GaluShop.exception.ProductNotFoundException;
+import pl.galushop.GaluShop.repository.OrderProductRepository;
+import pl.galushop.GaluShop.repository.OrderRepository;
+import pl.galushop.GaluShop.repository.ProductRepository;
 
 @Service
 @RequiredArgsConstructor
 public class OrderProductService {
+    private final OrderProductRepository orderProductRepository;
+    private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+
+    public void saveOrderProduct(Long orderId, Long productId, int quantity){
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException(""));
+        Product product = productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException(""));
+        OrderProduct orderProduct = new OrderProduct(order, product, quantity);
+        orderProductRepository.save(orderProduct);
+    }
 }
