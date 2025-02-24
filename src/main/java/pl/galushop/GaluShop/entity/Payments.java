9b129d02f481da1131c3f3a85d07bad711172aaf
michalgalus <galusmichal@outlook.com>
2025-02-24 14:02:37 +0100

    SCRUM-2 added fields to Payments entity

--- src/main/java/pl/galushop/GaluShop/entity/Payments.java
+++ src/main/java/pl/galushop/GaluShop/entity/Payments.java
@@ -1,11 +1,16 @@
 package pl.galushop.GaluShop.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.galushop.GaluShop.component.PaymentStatus;
 
 @Entity
 @Getter
@@ -14,5 +19,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Payments {
 
     @Id
-    Long paymentsId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Long paymentsId;
+
+    private Double price;
+
+    private PaymentStatus paymentStatus;
+
+    @OneToOne
+    private Order order;
 }
