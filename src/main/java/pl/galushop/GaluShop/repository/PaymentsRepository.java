d917af62bc52af8179461f84a8f2ea647ae95631
michalgalus <galusmichal@outlook.com>
2025-02-25 12:08:04 +0100

    SCRUM-2 added findByOrder_OrderId to PaymentsRepository

--- src/main/java/pl/galushop/GaluShop/repository/PaymentsRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/PaymentsRepository.java
@@ -3,5 +3,8 @@ package pl.galushop.GaluShop.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.galushop.GaluShop.entity.Payments;
 
+import java.util.Optional;
+
 public interface PaymentsRepository extends JpaRepository<Payments, Long> {
+    Optional<Payments> findByOrder_OrderId(Long orderId);
 }
