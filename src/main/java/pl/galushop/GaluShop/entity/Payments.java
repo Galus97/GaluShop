6bc45e7e8c9a4fdf54cd53fe60eaf1007781f215
michalgalus <galusmichal@outlook.com>
2025-02-25 12:06:30 +0100

    SCRUM-2 added PaymentsRepository interface

--- src/main/java/pl/galushop/GaluShop/entity/Payments.java
+++ src/main/java/pl/galushop/GaluShop/entity/Payments.java
@@ -9,7 +9,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.web.bind.annotation.GetMapping;
 import pl.galushop.GaluShop.component.PaymentStatus;
 
 @Entity
