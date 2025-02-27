db1f13dcb81d0e58629f7f0a9af513c73d23e1c3
michalgalus <galusmichal@outlook.com>
2025-02-27 13:00:33 +0100

    SCRUM-2 added showOrderPayment to PaymentOrderController

--- src/main/java/pl/galushop/GaluShop/controller/PaymentOrderController.java
+++ src/main/java/pl/galushop/GaluShop/controller/PaymentOrderController.java
@@ -1,11 +1,22 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.Payments;
+import pl.galushop.GaluShop.service.PaymentsService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/order/payment")
+@RequestMapping("/payment/order")
 public class PaymentOrderController {
+    private final PaymentsService paymentsService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Payments> showOrderPayment(@PathVariable Long id){
+        return ResponseEntity.ok(paymentsService.getPaymentByOrderId(id));
+    }
 }
