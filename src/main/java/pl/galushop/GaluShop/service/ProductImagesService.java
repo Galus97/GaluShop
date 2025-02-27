fa44beab1ebbb4feef494e13aa9eafd55440be21
michalgalus <galusmichal@outlook.com>
2025-02-27 18:39:15 +0100

    SCRUM-3 added deleteProductImages to ProductImagesService

--- src/main/java/pl/galushop/GaluShop/service/ProductImagesService.java
+++ src/main/java/pl/galushop/GaluShop/service/ProductImagesService.java
@@ -53,24 +53,16 @@ public class ProductImagesService {
 
 
     public void deleteProductImages(Long imagesId) {
-        if (imagesId != null && imagesId > 0) {
-            if (productImagesRepository.existsById(imagesId)) {
-                productImagesRepository.deleteById(imagesId);
-            } else {
-                throw new NoSuchElementException("That Image doesn't exist in database");
-            }
-        } else {
-            throw new IllegalArgumentException("Image Product Id is invalid");
+        if(imagesId == null || imagesId < 0){
+            throw new IllegalArgumentException(messageService.getMessage("error.invalidProductImagesId", imagesId));
         }
+        ProductImages productImages = productImagesRepository.findById(imagesId)
+                .orElseThrow(() -> new ProductImagesNotFoundException(messageService.getMessage("error.productImagesNotFoundException", imagesId)));
+        productImagesRepository.delete(productImages);
     }
 
+    //return productImagesRepository.findAllByProduct_ProductId(productId);
     public List<ProductImages> getAllImagesByProductId(Long productId) {
-        if (productId != null && productId > 0) {
-            if (productImagesRepository.existsByProduct_ProductId(productId)) {
-                return productImagesRepository.findAllByProduct_ProductId(productId);
-            }
-            throw new NoSuchElementException("Images doesn't exist in database");
-        }
-        throw new IllegalArgumentException("Product Id is invalid");
+
     }
 }
