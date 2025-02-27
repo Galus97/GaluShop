ca5ae72628653266e3dd6ef6f47eb296235c0710
michalgalus <galusmichal@outlook.com>
2025-02-27 18:23:26 +0100

    SCRUM-3 added ProductImagesNotFoundException class

---
+++ src/main/java/pl/galushop/GaluShop/exception/ProductImagesNotFoundException.java
@@ -0,0 +1,7 @@
+package pl.galushop.GaluShop.exception;
+
+public class ProductImagesNotFoundException extends RuntimeException{
+    public ProductImagesNotFoundException(String message){
+        super(message);
+    }
+}
