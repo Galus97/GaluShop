3392790044a1166a4ecb62220193da8d9d2026bd
michalgalus <galusmichal@outlook.com>
2025-02-27 12:48:17 +0100

    SCRUM-2 added showPayments to PaymentsController

--- src/main/java/pl/galushop/GaluShop/controller/PaymentsController.java
+++ src/main/java/pl/galushop/GaluShop/controller/PaymentsController.java
@@ -1,11 +1,24 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.Payments;
+import pl.galushop.GaluShop.service.PaymentsService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/payments")
 public class PaymentsController {
+    private final PaymentsService paymentsService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Payments> showPayments(@PathVariable Long id){
+        return ResponseEntity.ok(paymentsService.getPaymentById(id));
+    }
+
+
 }
