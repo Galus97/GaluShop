0a4a1cc56b15c709762a39d7ee3179d2aa730da7
michalgalus <galusmichal@outlook.com>
2025-02-28 08:13:06 +0100

    SCRUM-3 added showProductImages to ProductImagesController

--- src/main/java/pl/galushop/GaluShop/controller/ProductImagesController.java
+++ src/main/java/pl/galushop/GaluShop/controller/ProductImagesController.java
@@ -1,8 +1,12 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.ProductImages;
 import pl.galushop.GaluShop.service.ProductImagesService;
 
 @RestController
@@ -10,4 +14,9 @@ import pl.galushop.GaluShop.service.ProductImagesService;
 @RequestMapping("/images")
 public class ProductImagesController {
     private final ProductImagesService productImagesService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductImages> showProductImages(@PathVariable Long id){
+        return ResponseEntity.ok(productImagesService.getProductImages(id));
+    }
 }
