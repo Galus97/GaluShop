921927569a7d77e7daa382f81e715ddfdb8f375d
michalgalus <galusmichal@outlook.com>
2025-02-28 16:22:56 +0100

    SCRUM-3  added documentation comments to MessageService

--- src/main/java/pl/galushop/GaluShop/component/MessageService.java
+++ src/main/java/pl/galushop/GaluShop/component/MessageService.java
@@ -7,11 +7,21 @@ import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
+/**
+ * Service component responsible for retrieving localized messages.
+ */
 @Component
 @RequiredArgsConstructor
 public class MessageService {
     private final MessageSource messageSource;
 
+    /**
+     * Retrieves a localized message based on the given key and parameters.
+     *
+     * @param key    The message key.
+     * @param params Optional parameters to format the message.
+     * @return The localized message as a string.
+     */
     public String getMessage(String key, Object... params){
         Locale locale = LocaleContextHolder.getLocale();
         return messageSource.getMessage(key, params, locale);
