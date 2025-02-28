b96e9677b44975d926e0ff3ecffc2ee6301b013a
michalgalus <galusmichal@outlook.com>
2025-02-28 08:29:19 +0100

    SCRUM-3 added saveProductImages to ProductImagesController

--- src/main/java/pl/galushop/GaluShop/controller/ProductImagesController.java
+++ src/main/java/pl/galushop/GaluShop/controller/ProductImagesController.java
@@ -4,10 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.dto.ProductImageRequest;
 import pl.galushop.GaluShop.entity.ProductImages;
 import pl.galushop.GaluShop.service.ProductImagesService;
+import pl.galushop.GaluShop.service.ProductService;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +22,11 @@ public class ProductImagesController {
     @GetMapping("/{id}")
     public ResponseEntity<ProductImages> showProductImages(@PathVariable Long id){
         return ResponseEntity.ok(productImagesService.getProductImages(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductImages> saveProductImages(@RequestBody ProductImageRequest productImageRequest){
+        productImagesService.saveProductImages(productImageRequest);
+        return ResponseEntity.ok(productImagesService.getProductImages(productImageRequest.getImagesId()));
     }
 }
