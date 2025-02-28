dcdfed18dee9ec823156d9023fbbe701af30907a
michalgalus <galusmichal@outlook.com>
2025-02-28 16:21:20 +0100

    SCRUM-3  added documentation comments to CurrentUser

--- src/main/java/pl/galushop/GaluShop/component/CurrentUser.java
+++ src/main/java/pl/galushop/GaluShop/component/CurrentUser.java
@@ -4,10 +4,22 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
+/**
+ * Custom implementation of Spring Security's {@link User} class
+ * to include additional user details.
+ */
 @Getter
 public class CurrentUser extends User {
     private final pl.galushop.GaluShop.entity.User user;
 
+    /**
+     * Constructs a {@code CurrentEmployee} instance with the given details.
+     *
+     * @param username    The username of the employee.
+     * @param password    The encoded password of the employee.
+     * @param authorities The granted authorities for the employee.
+     * @param user    The employee entity associated with this user.
+     */
     public CurrentUser(String username, String password, java.util.Collection<?
             extends GrantedAuthority> authorities, pl.galushop.GaluShop.entity.User user) {
 
