2556896d4ce8384b42bfbad45e2c272debbb14fc
michalgalus <galusmichal@outlook.com>
2025-02-28 08:32:24 +0100

    SCRUM-3 added deleteProductImages to ProductImagesController

--- src/main/java/pl/galushop/GaluShop/controller/ProductImagesController.java
+++ src/main/java/pl/galushop/GaluShop/controller/ProductImagesController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.ProductImageRequest;
 import pl.galushop.GaluShop.entity.ProductImages;
 import pl.galushop.GaluShop.service.ProductImagesService;
-import pl.galushop.GaluShop.service.ProductService;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,24 +21,24 @@ public class ProductImagesController {
     private final ProductImagesService productImagesService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductImages> showProductImages(@PathVariable Long id){
+    public ResponseEntity<ProductImages> showProductImages(@PathVariable Long id) {
         return ResponseEntity.ok(productImagesService.getProductImages(id));
     }
 
     @PostMapping
-    public ResponseEntity<ProductImages> saveProductImages(@RequestBody ProductImageRequest productImageRequest){
+    public ResponseEntity<ProductImages> saveProductImages(@RequestBody ProductImageRequest productImageRequest) {
         productImagesService.saveProductImages(productImageRequest);
         return ResponseEntity.ok(productImagesService.getProductImages(productImageRequest.getImagesId()));
     }
 
     @PutMapping
-    public ResponseEntity<ProductImages> updateProductImages(@RequestBody ProductImageRequest productImageRequest){
+    public ResponseEntity<ProductImages> updateProductImages(@RequestBody ProductImageRequest productImageRequest) {
         productImagesService.updateProductImages(productImageRequest);
         return ResponseEntity.ok(productImagesService.getProductImages(productImageRequest.getImagesId()));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteProductImages(@PathVariable Long id){
+    public ResponseEntity<Void> deleteProductImages(@PathVariable Long id) {
         productImagesService.deleteProductImages(id);
         return ResponseEntity.noContent().build();
     }
