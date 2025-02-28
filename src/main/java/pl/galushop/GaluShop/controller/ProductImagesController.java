294ac1eaac5345eb00149af8ee747fa194bb15ef
michalgalus <galusmichal@outlook.com>
2025-02-28 08:32:08 +0100

    SCRUM-3 added deleteProductImages to ProductImagesController

--- src/main/java/pl/galushop/GaluShop/controller/ProductImagesController.java
+++ src/main/java/pl/galushop/GaluShop/controller/ProductImagesController.java
@@ -2,6 +2,7 @@ package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,5 +36,11 @@ public class ProductImagesController {
     public ResponseEntity<ProductImages> updateProductImages(@RequestBody ProductImageRequest productImageRequest){
         productImagesService.updateProductImages(productImageRequest);
         return ResponseEntity.ok(productImagesService.getProductImages(productImageRequest.getImagesId()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProductImages(@PathVariable Long id){
+        productImagesService.deleteProductImages(id);
+        return ResponseEntity.noContent().build();
     }
 }
