f7e6a81181dda97e5dcf4d40fdbda0baea52694c
michalgalus <galusmichal@outlook.com>
2025-02-28 23:46:34 +0100

    SCRUM-3 added updateQuantity to WarehouseUpdateQuantityController

--- src/main/java/pl/galushop/GaluShop/controller/WarehouseUpdateQuantityController.java
+++ src/main/java/pl/galushop/GaluShop/controller/WarehouseUpdateQuantityController.java
@@ -1,8 +1,12 @@
 package pl.galushop.GaluShop.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.WarehouseProduct;
 import pl.galushop.GaluShop.service.WarehouseProductService;
 
 @RestController
@@ -10,4 +14,10 @@ import pl.galushop.GaluShop.service.WarehouseProductService;
 @RequestMapping("/warehouse")
 public class WarehouseUpdateQuantityController {
     private final WarehouseProductService warehouseProductService;
+
+    @PutMapping("/{id}/{quantity}")
+    public ResponseEntity<WarehouseProduct> updateQuantity(@PathVariable Long id, @PathVariable Integer quantity){
+        warehouseProductService.updateQuantityByProductId(id, quantity);
+        return ResponseEntity.ok(warehouseProductService.getWarehouseProduct(id));
+    }
 }
