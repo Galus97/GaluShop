2787eb3dfe0cbb6c717cca624851c69819e03944
michalgalus <galusmichal@outlook.com>
2025-03-01 13:57:16 +0100

    SCRUM-3 added ProductFacadeService

---
+++ src/main/java/pl/galushop/GaluShop/service/ProductFacadeService.java
@@ -0,0 +1,12 @@
+package pl.galushop.GaluShop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Facade service that manages interactions between ProductService and ProductImagesService.
+ */
+@Service
+@RequiredArgsConstructor
+public class ProductFacadeService {
+}
