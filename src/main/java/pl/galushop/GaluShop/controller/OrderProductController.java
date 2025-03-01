73a1cd4be6c817c0bcdd0d66c43bcf264b826f78
michalgalus <galusmichal@outlook.com>
2025-03-01 17:37:22 +0100

    SCRUM-3 added showProductInOrder to OrderProductController

---
+++ src/main/java/pl/galushop/GaluShop/controller/OrderProductController.java
@@ -0,0 +1,24 @@
+package pl.galushop.GaluShop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.galushop.GaluShop.entity.OrderProduct;
+import pl.galushop.GaluShop.service.OrderProductService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/order")
+public class OrderProductController {
+    private final OrderProductService orderProductService;
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<List<OrderProduct>> showProductInOrder(@PathVariable Long id){
+        return ResponseEntity.ok(orderProductService.getOrderProductsByOrderId(id));
+    }
+}
