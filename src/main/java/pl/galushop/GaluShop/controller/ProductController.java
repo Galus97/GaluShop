0c454acd23ac7cc0e9f99ecb2a3a8803236e12c8
michalgalus <galusmichal@outlook.com>
2025-03-01 14:14:23 +0100

    SCRUM-3 added ProductFacadeService to ProductController

--- src/main/java/pl/galushop/GaluShop/controller/ProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.ProductRequest;
 import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.service.ProductFacadeService;
 import pl.galushop.GaluShop.service.ProductService;
 
 @RestController
@@ -19,6 +20,7 @@ import pl.galushop.GaluShop.service.ProductService;
 @RequestMapping("/product")
 public class ProductController {
     private final ProductService productService;
+    private final ProductFacadeService productFacadeService;
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> showProduct(@PathVariable Long id){
@@ -27,7 +29,7 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<Product> saveProduct(@RequestBody ProductRequest productRequest) {
-        productService.saveProduct(productRequest);
+        productFacadeService.saveProductWithImages(productRequest);
         return ResponseEntity.ok(productService.getProduct(productRequest.getProductId()));
     }
 
