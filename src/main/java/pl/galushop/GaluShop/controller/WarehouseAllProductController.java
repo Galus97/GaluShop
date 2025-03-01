3ab520bd9cf4b52141a21941d99ecc223aa011e9
michalgalus <galusmichal@outlook.com>
2025-03-01 14:44:22 +0100

    SCRUM-3 change name to WarehouseAllProductController

--- src/main/java/pl/galushop/GaluShop/controller/WarehouseAllProductController.java
+++ src/main/java/pl/galushop/GaluShop/controller/WarehouseAllProductController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/warehouse")
-public class AllWarehouseProductController {
+public class WarehouseAllProductController {
     private final WarehouseProductService warehouseProductService;
 
 
