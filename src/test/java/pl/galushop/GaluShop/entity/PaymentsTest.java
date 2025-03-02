19ca07824ec47bcaad418f7f627f87b6349a1165
michalgalus <galusmichal@outlook.com>
2025-03-03 00:46:00 +0100

    SCRUM-4 added setUp to PaymentsTest

--- src/test/java/pl/galushop/GaluShop/entity/PaymentsTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/PaymentsTest.java
@@ -1,7 +1,43 @@
 package pl.galushop.GaluShop.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import pl.galushop.GaluShop.component.OrderStatus;
+import pl.galushop.GaluShop.component.PaymentStatus;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
 
 class PaymentsTest {
+    private Payments payments;
+    private Order order;
+    private User user;
+    private Validator validator;
 
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+
+        user = new User();
+        user.setUserId(1L);
+
+        order = Order.builder()
+                .orderId(1L)
+                .localDateTime(LocalDateTime.now())
+                .status(OrderStatus.PLACED)
+                .user(user)
+                .orderProducts(Collections.emptyList())
+                .build();
+
+        payments = Payments.builder()
+                .paymentId(1L)
+                .totalAmount(100.0)
+                .paymentStatus(PaymentStatus.PENDING)
+                .order(order)
+                .user(user)
+                .build();
+    }
 }
