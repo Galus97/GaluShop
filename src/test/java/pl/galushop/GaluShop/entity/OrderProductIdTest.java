1f2b988ac8b56a7693de3b5ab672c9ec52ccd569
michalgalus <galusmichal@outlook.com>
2025-03-03 00:31:49 +0100

    SCRUM-4 added shouldCreateOrderProductIdUsingConstructor to OrderProductIdTest

--- src/test/java/pl/galushop/GaluShop/entity/OrderProductIdTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/OrderProductIdTest.java
@@ -1,6 +1,9 @@
 package pl.galushop.GaluShop.entity;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class OrderProductIdTest {
     private OrderProductId orderProductId1;
@@ -10,5 +13,12 @@ class OrderProductIdTest {
     void setUp() {
         orderProductId1 = new OrderProductId(1L, 2L);
         orderProductId2 = new OrderProductId(1L, 2L);
+    }
+
+    @Test
+    void shouldCreateOrderProductIdUsingConstructor() {
+        assertThat(orderProductId1).isNotNull();
+        assertThat(orderProductId1.getOrderId()).isEqualTo(1L);
+        assertThat(orderProductId1.getProductId()).isEqualTo(2L);
     }
 }
