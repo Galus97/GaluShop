6ae8237de69e7ca037a412560437f2398ee0e64d
michalgalus <galusmichal@outlook.com>
2025-03-03 00:25:29 +0100

    SCRUM-4 added setUp to OrderProductIdTest

--- src/test/java/pl/galushop/GaluShop/entity/OrderProductIdTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/OrderProductIdTest.java
@@ -1,7 +1,14 @@
 package pl.galushop.GaluShop.entity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 class OrderProductIdTest {
+    private OrderProductId orderProductId1;
+    private OrderProductId orderProductId2;
 
+    @BeforeEach
+    void setUp() {
+        orderProductId1 = new OrderProductId(1L, 2L);
+        orderProductId2 = new OrderProductId(1L, 2L);
+    }
 }
