5009b7a3b27a9527c2a6ea16c6c308714cc8da52
michalgalus <galusmichal@outlook.com>
2025-03-02 23:01:22 +0100

    SCRUM-4 added shouldCreateOrderProductUsingBuilder to OrderProductTest

--- src/test/java/pl/galushop/GaluShop/entity/OrderProductTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/OrderProductTest.java
@@ -1,6 +1,7 @@
 package pl.galushop.GaluShop.entity;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import pl.galushop.GaluShop.component.OrderStatus;
 
 import java.time.LocalDateTime;
@@ -19,7 +20,7 @@ class OrderProductTest {
     @BeforeEach
     void setUp() {
         fixedDateTime = LocalDateTime.of(2023, 10, 10, 12, 30);
-
+        OrderProductId id = new OrderProductId();
         order = Order.builder()
                 .orderId(1L)
                 .localDateTime(fixedDateTime)
@@ -38,9 +39,22 @@ class OrderProductTest {
                 .build();
 
         orderProduct = OrderProduct.builder()
+                .id(id)
                 .order(order)
                 .product(product)
                 .quantity(5)
                 .build();
+
+        id.setProductId(product.getProductId());
+        id.setOrderId(order.getOrderId());
+    }
+
+    @Test
+    void shouldCreateOrderProductUsingBuilder() {
+        assertNotNull(orderProduct);
+        assertNotNull(orderProduct.getOrder());
+        assertNotNull(orderProduct.getProduct());
+        assertNotNull(orderProduct.getId());
+        assertEquals(5, orderProduct.getQuantity());
     }
 }
