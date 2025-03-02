6e43612441849c9ee5f208b6ecc4c8d26fc95b3a
michalgalus <galusmichal@outlook.com>
2025-03-02 11:21:00 +0100

    SCRUM-4 added setUp & shouldCreateEmployeeUsingBuilder to EmployeeTest

--- src/test/java/pl/galushop/GaluShop/entity/EmployeeTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/EmployeeTest.java
@@ -1,10 +1,45 @@
 package pl.galushop.GaluShop.entity;
 
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 class EmployeeTest {
+    private static Validator validator;
+    private Employee employee;
 
+    @BeforeEach
+    void setUp() {
+        if (validator == null) {
+            ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+            validator = factory.getValidator();
+        }
+        employee = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .password("securePassword")
+                .enabled(true)
+                .emailCode("123456")
+                .build();
+    }
+
+    @Test
+    void shouldCreateEmployeeUsingBuilder() {
+        // Then
+        assertThat(employee).isNotNull();
+        assertThat(employee.getFirstName()).isEqualTo("John");
+        assertThat(employee.getLastName()).isEqualTo("Doe");
+        assertThat(employee.getEmail()).isEqualTo("john.doe@example.com");
+        assertThat(employee.getPassword()).isEqualTo("securePassword");
+        assertThat(employee.isEnabled()).isTrue();
+        assertThat(employee.getEmailCode()).isEqualTo("123456");
+    }
 
 
 }
