1c840bb17f9ed92f8f4cc25d5e64f5ff96ede2a4
michalgalus <galusmichal@outlook.com>
2025-03-03 01:32:12 +0100

    SCRUM-4 added setUp to ProductTest

--- src/test/java/pl/galushop/GaluShop/entity/ProductTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/ProductTest.java
@@ -1,7 +1,24 @@
 package pl.galushop.GaluShop.entity;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Collections;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductTest {
+    private Product product;
 
+    @BeforeEach
+    void setUp() {
+        product = Product.builder()
+                .productId(1L)
+                .productName("Sample Product")
+                .description("This is a sample product description with enough length.")
+                .price(100.0)
+                .category("Electronics")
+                .categoryId(2)
+                .orderProducts(Collections.emptyList())
+                .build();
+    }
 }
