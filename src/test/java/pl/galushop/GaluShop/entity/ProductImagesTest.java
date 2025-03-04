93186d4c1c67d8149768e9587810d20b5a88e205
michalgalus <galusmichal@outlook.com>
2025-03-04 14:44:41 +0100

    SCRUM-4 added ProductImagesTest

---
+++ src/test/java/pl/galushop/GaluShop/entity/ProductImagesTest.java
@@ -0,0 +1,12 @@
+package pl.galushop.GaluShop.entity;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductImagesTest {
+    
+}
