3cf4914c071fba23d79b712ea3ea96d1edba087d
michalgalus <galusmichal@outlook.com>
2025-03-04 15:31:17 +0100

    SCRUM-4 added shouldDetectBlankImgSrc ProductImagesTest

--- src/test/java/pl/galushop/GaluShop/entity/ProductImagesTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/ProductImagesTest.java
@@ -1,10 +1,13 @@
 package pl.galushop.GaluShop.entity;
 
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,5 +31,21 @@ class ProductImagesTest {
         assertThat(productImages).isNotNull();
         assertThat(productImages.getImgSrc()).isEqualTo("https://example.com/image.jpg");
         assertThat(productImages.getAltImg()).isEqualTo("Sample Image");
+    }
+
+    @Test
+    void shouldDetectBlankImgSrc() {
+        // Given
+        ProductImages productImages = ProductImages.builder()
+                .imgSrc("") // Invalid blank field
+                .altImg("Valid Alt Text")
+                .build();
+
+        // When
+        Set<ConstraintViolation<ProductImages>> violations = validator.validate(productImages);
+
+        // Then
+        assertEquals(1, violations.size());
+        assertThat(violations.iterator().next().getMessage()).isEqualTo("must not be blank");
     }
 }
