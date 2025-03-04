f2ddb7774c2987e21283fcae3337301c6d4f66c9
michalgalus <galusmichal@outlook.com>
2025-03-04 15:45:03 +0100

    SCRUM-4 added shouldCreateUserSuccessfully to UserTest

--- src/test/java/pl/galushop/GaluShop/entity/UserTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/UserTest.java
@@ -4,7 +4,9 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
@@ -14,5 +16,25 @@ class UserTest {
     static void setUp() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+    }
+
+    @Test
+    void shouldCreateUserSuccessfully() {
+        User user = User.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .password("securePassword")
+                .enabled(true)
+                .emailCode("123456")
+                .build();
+
+        assertThat(user).isNotNull();
+        assertThat(user.getFirstName()).isEqualTo("John");
+        assertThat(user.getLastName()).isEqualTo("Doe");
+        assertThat(user.getEmail()).isEqualTo("john.doe@example.com");
+        assertThat(user.getPassword()).isEqualTo("securePassword");
+        assertThat(user.isEnabled()).isTrue();
+        assertThat(user.getEmailCode()).isEqualTo("123456");
     }
 }
