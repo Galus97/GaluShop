3b1362bbcf35ccaf369abe6970f7a83ece9a9194
michalgalus <galusmichal@outlook.com>
2025-03-04 15:05:57 +0100

    SCRUM-4 added shouldCreateProductImagesSuccessfully ProductImagesTest

--- src/test/java/pl/galushop/GaluShop/entity/ProductImagesTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/ProductImagesTest.java
@@ -4,7 +4,9 @@ import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductImagesTest {
@@ -14,5 +16,17 @@ class ProductImagesTest {
     static void setUp() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         validator = factory.getValidator();
+    }
+
+    @Test
+    void shouldCreateProductImagesSuccessfully() {
+        ProductImages productImages = ProductImages.builder()
+                .imgSrc("https://example.com/image.jpg")
+                .altImg("Sample Image")
+                .build();
+
+        assertThat(productImages).isNotNull();
+        assertThat(productImages.getImgSrc()).isEqualTo("https://example.com/image.jpg");
+        assertThat(productImages.getAltImg()).isEqualTo("Sample Image");
     }
 }
