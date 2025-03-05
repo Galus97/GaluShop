5f645c604ba48170d179d509d925842b2c7cbd76
michalgalus <galusmichal@outlook.com>
2025-03-05 09:10:54 +0100

    SCRUM-4 added shouldCreateWarehouseProductWithValidData to WarehouseProductTest

--- src/test/java/pl/galushop/GaluShop/entity/WarehouseProductTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/WarehouseProductTest.java
@@ -1,7 +1,9 @@
 package pl.galushop.GaluShop.entity;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseProductTest {
@@ -12,4 +14,17 @@ class WarehouseProductTest {
         product = new Product();
     }
 
+    @Test
+    void shouldCreateWarehouseProductWithValidData() {
+        WarehouseProduct warehouseProduct = WarehouseProduct.builder()
+                .warehouseProductId(1L)
+                .product(product)
+                .quantity(10)
+                .build();
+
+        assertThat(warehouseProduct).isNotNull();
+        assertThat(warehouseProduct.getWarehouseProductId()).isEqualTo(1L);
+        assertThat(warehouseProduct.getProduct()).isEqualTo(product);
+        assertThat(warehouseProduct.getQuantity()).isEqualTo(10);
+    }
 }
