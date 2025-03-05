33dc6c00a56652b1bf5890979fc2bd755a1fccd4
michalgalus <galusmichal@outlook.com>
2025-03-05 09:15:25 +0100

    SCRUM-4 added shouldNotAllowNegativeQuantity to WarehouseProductTest

--- src/test/java/pl/galushop/GaluShop/entity/WarehouseProductTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/WarehouseProductTest.java
@@ -1,10 +1,15 @@
 package pl.galushop.GaluShop.entity;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseProductTest {
     private Product product;
@@ -26,5 +31,22 @@ class WarehouseProductTest {
         assertThat(warehouseProduct.getWarehouseProductId()).isEqualTo(1L);
         assertThat(warehouseProduct.getProduct()).isEqualTo(product);
         assertThat(warehouseProduct.getQuantity()).isEqualTo(10);
+    }
+
+    @Test
+    void shouldNotAllowNegativeQuantity() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+
+        WarehouseProduct warehouseProduct = WarehouseProduct.builder()
+                    .warehouseProductId(2L)
+                    .product(product)
+                    .quantity(-1) // Invalid value
+                    .build();
+
+        Set<ConstraintViolation<WarehouseProduct>> violations = validator.validate(warehouseProduct);
+
+        assertThat(violations).isNotEmpty();
+        assertThat(violations).extracting("message").contains("must be greater than or equal to 0");
     }
 }
