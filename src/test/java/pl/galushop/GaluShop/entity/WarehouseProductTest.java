c2d92a9e503337bbe3680fd3f35dd90bba9bbe7e
michalgalus <galusmichal@outlook.com>
2025-03-05 09:09:51 +0100

    SCRUM-4 added setUp to WarehouseProductTest

--- src/test/java/pl/galushop/GaluShop/entity/WarehouseProductTest.java
+++ src/test/java/pl/galushop/GaluShop/entity/WarehouseProductTest.java
@@ -1,7 +1,15 @@
 package pl.galushop.GaluShop.entity;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseProductTest {
+    private Product product;
+
+    @BeforeEach
+    void setUp() {
+        product = new Product();
+    }
 
 }
