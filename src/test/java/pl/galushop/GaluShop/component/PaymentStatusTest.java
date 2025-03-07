7e9b488447324cf9844f9e2aefb33446db481ad3
michalgalus <galusmichal@outlook.com>
2025-03-07 18:23:47 +0100

    SCRUM-4 added shouldContainAllRequiredStatusesInCorrectOrder to PaymentStatusTest

--- src/test/java/pl/galushop/GaluShop/component/PaymentStatusTest.java
+++ src/test/java/pl/galushop/GaluShop/component/PaymentStatusTest.java
@@ -7,5 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentStatusTest {
 
+    @Test
+    void shouldContainAllRequiredStatusesInCorrectOrder() {
+        PaymentStatus[] values = PaymentStatus.values();
+
+        assertThat(values)
+                .containsExactly(
+                        PaymentStatus.NEW,
+                        PaymentStatus.PENDING,
+                        PaymentStatus.CONFIRMED,
+                        PaymentStatus.REJECTED,
+                        PaymentStatus.ABANDONED,
+                        PaymentStatus.EXPIRED,
+                        PaymentStatus.ERROR
+                );
+    }
 
 }
