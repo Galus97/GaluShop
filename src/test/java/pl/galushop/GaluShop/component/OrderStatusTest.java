59a06ecbe37bb04ae38712c8ed2a7c5cf4054b86
michalgalus <galusmichal@outlook.com>
2025-03-07 18:28:16 +0100

    SCRUM-4 added shouldContainAllOrderLifecycleStatuses to OrderStatusTest

--- src/test/java/pl/galushop/GaluShop/component/OrderStatusTest.java
+++ src/test/java/pl/galushop/GaluShop/component/OrderStatusTest.java
@@ -1,7 +1,22 @@
 package pl.galushop.GaluShop.component;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderStatusTest {
 
+    @Test
+    void shouldContainAllOrderLifecycleStatuses() {
+        OrderStatus[] values = OrderStatus.values();
+
+        assertThat(values)
+                .containsExactly(
+                        OrderStatus.PLACED,
+                        OrderStatus.PAID,
+                        OrderStatus.PROCESSED,
+                        OrderStatus.SENT
+                );
+    }
 }
