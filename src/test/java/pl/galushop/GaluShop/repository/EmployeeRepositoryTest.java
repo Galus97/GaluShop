710b95563c945bdbfd7ff5a48da936b585706695
michalgalus <galusmichal@outlook.com>
2025-03-10 22:22:55 +0100

    SCRUM-4 added EmployeeRepositoryTest.java

---
+++ src/test/java/pl/galushop/GaluShop/repository/EmployeeRepositoryTest.java
@@ -0,0 +1,16 @@
+package pl.galushop.GaluShop.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class EmployeeRepositoryTest {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    TestEntityManager testEntityManager;
+}
