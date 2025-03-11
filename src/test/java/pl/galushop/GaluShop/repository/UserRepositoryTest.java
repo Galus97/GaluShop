3b1184432418e0c4298991c132cfefd9eda37d99
michalgalus <galusmichal@outlook.com>
2025-03-11 19:30:51 +0100

    SCRUM-4 added setUp to UserRepositoryTest

--- src/test/java/pl/galushop/GaluShop/repository/UserRepositoryTest.java
+++ src/test/java/pl/galushop/GaluShop/repository/UserRepositoryTest.java
@@ -1,10 +1,32 @@
 package pl.galushop.GaluShop.repository;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import pl.galushop.GaluShop.entity.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class UserRepositoryTest {
 
+    @Autowired
+    TestEntityManager entityManager;
+    @Autowired
+    UserRepository userRepository;
+    private User user;
+
+    @BeforeEach
+    void setUp(){
+        user = User.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@gmail.com")
+                .password("{noop}password")
+                .enabled(true)
+                .emailCode("1111")
+                .build();
+        entityManager.persistAndFlush(user);
+    }
 }
