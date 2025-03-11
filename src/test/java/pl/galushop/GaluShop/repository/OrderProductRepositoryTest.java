dc376e9426ddb96ddcf87d0260c81f73a7dddcc8
michalgalus <galusmichal@outlook.com>
2025-03-11 12:31:03 +0100

    SCRUM-4 added OrderProductRepositoryTest.java

---
+++ src/test/java/pl/galushop/GaluShop/repository/OrderProductRepositoryTest.java
@@ -0,0 +1,18 @@
+package pl.galushop.GaluShop.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class OrderProductRepositoryTest {
+
+    @Autowired
+    TestEntityManager testEntityManager;
+    @Autowired
+    OrderProductRepository orderProductRepository;
+
+
+}
