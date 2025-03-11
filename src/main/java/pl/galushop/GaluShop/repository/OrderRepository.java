cd26a05369ec169ce723f18f092997c4b1f56754
michalgalus <galusmichal@outlook.com>
2025-03-11 14:41:37 +0100

    SCRUM-4 deleted findAllByUser_UserId from OrderRepository

--- src/main/java/pl/galushop/GaluShop/repository/OrderRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/OrderRepository.java
@@ -1,20 +1,11 @@
 package pl.galushop.GaluShop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
-import pl.galushop.GaluShop.component.OrderStatus;
 import pl.galushop.GaluShop.entity.Order;
-import pl.galushop.GaluShop.entity.Product;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Optional<List<Order>> findAllByUser_UserId(Long userId);
-
-    Optional<Order> findByUser_UserId(Long userId);
+    List<Order> findAllByUser_UserId(Long userId);
 }
