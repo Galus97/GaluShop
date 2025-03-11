e6c571f865dbd0b9409aa6ed49159443a499979d
michalgalus <galusmichal@outlook.com>
2025-03-11 23:27:28 +0100

    SCRUM-4 added givenExistingUser_whenLoadUserByUsername_thenReturnUserDetails to CustomUserDetailsServiceTest

--- src/test/java/pl/galushop/GaluShop/service/CustomUserDetailsServiceTest.java
+++ src/test/java/pl/galushop/GaluShop/service/CustomUserDetailsServiceTest.java
@@ -1,17 +1,26 @@
 package pl.galushop.GaluShop.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
 import pl.galushop.GaluShop.component.MessageService;
 import pl.galushop.GaluShop.entity.Employee;
 import pl.galushop.GaluShop.entity.User;
 import pl.galushop.GaluShop.repository.EmployeeRepository;
 import pl.galushop.GaluShop.repository.UserRepository;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomUserDetailsServiceTest {
@@ -47,5 +56,18 @@ class CustomUserDetailsServiceTest {
                 .enabled(true)
                 .emailCode("2222")
                 .build();
+    }
+
+    @Test
+    void givenExistingUser_whenLoadUserByUsername_thenReturnUserDetails(){
+        // Arrange
+        when(userRepository.findByEmail("john.doe@gmail.com")).thenReturn(Optional.of(user));
+        //Act
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername("john.doe@gmail.com");
+        //Assert
+        assertThat(userDetails).isNotNull();
+        assertThat(userDetails.getUsername()).isEqualTo("john.doe@gmail.com");
+        verify(userRepository, times(1)).findByEmail("john.doe@gmail.com");
+        verifyNoInteractions(employeeRepository);
     }
 }
