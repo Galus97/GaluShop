dff9e11e38beceaf52ddd923b2c936504844b640
michalgalus <galusmichal@outlook.com>
2025-03-11 17:59:47 +0100

    SCRUM-4 deleted unused imports form ProductRepository

--- src/main/java/pl/galushop/GaluShop/repository/UserDataRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/UserDataRepository.java
@@ -1,14 +1,10 @@
 package pl.galushop.GaluShop.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 import pl.galushop.GaluShop.entity.UserData;
 
 import java.util.Optional;
 
 public interface UserDataRepository extends JpaRepository<UserData, Long> {
-
     Optional<UserData> findByUser_UserId(Long id);
 }
