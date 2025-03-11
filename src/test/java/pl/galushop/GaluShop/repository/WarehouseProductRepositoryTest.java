02a4f5f949c9e5a4bf944737f5b1df39ae066889
michalgalus <galusmichal@outlook.com>
2025-03-11 22:25:27 +0100

    SCRUM-4 added setUp to WarehouseProductRepositoryTest

--- src/test/java/pl/galushop/GaluShop/repository/WarehouseProductRepositoryTest.java
+++ src/test/java/pl/galushop/GaluShop/repository/WarehouseProductRepositoryTest.java
@@ -1,10 +1,38 @@
 package pl.galushop.GaluShop.repository;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.entity.WarehouseProduct;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 class WarehouseProductRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+    @Autowired
+    WarehouseProductRepository warehouseProductRepository;
+    private WarehouseProduct warehouseProduct;
+
+    @BeforeEach
+    void setUp(){
+        Product persistedProduct = entityManager.persistAndFlush(Product.builder()
+                .productName("Phone")
+                .description("Valid description of phone")
+                .price(550d)
+                .category("Category 1")
+                .categoryId(1)
+                .build());
+        warehouseProduct = WarehouseProduct.builder()
+                .product(persistedProduct)
+                .quantity(10)
+                .build();
+        entityManager.persistAndFlush(warehouseProduct);
+    }
+
 
 }
