b847ac3d76802d4cdd3b151553529af00a54c308
michalgalus <galusmichal@outlook.com>
2025-03-11 17:46:01 +0100

    SCRUM-4 deleted usused method from ProductImagesRepository

--- src/main/java/pl/galushop/GaluShop/repository/ProductImagesRepository.java
+++ src/main/java/pl/galushop/GaluShop/repository/ProductImagesRepository.java
@@ -8,6 +8,4 @@ import java.util.List;
 public interface ProductImagesRepository extends JpaRepository<ProductImages, Long> {
 
     List<ProductImages> findAllByProduct_ProductId(Long productId);
-
-    boolean existsByProduct_ProductId(Long productId);
 }
