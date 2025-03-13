beba038d25279bc520c41a2e742da4049dfe2af0
michalgalus <galusmichal@outlook.com>
2025-03-13 22:34:00 +0100

    SCRUM-4 added OrderResponse.java

---
+++ src/main/java/pl/galushop/GaluShop/dto/OrderResponse.java
@@ -0,0 +1,19 @@
+package pl.galushop.GaluShop.dto;
+
+import pl.galushop.GaluShop.component.OrderStatus;
+import pl.galushop.GaluShop.entity.Order;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record OrderResponse(Long orderId, LocalDateTime localDateTime, OrderStatus status, Long userId, List<OrderProductDto> products) {
+    public static OrderResponse fromEntity(Order order) {
+        return new OrderResponse(
+                order.getOrderId(),
+                order.getLocalDateTime(),
+                order.getStatus(),
+                order.getUser().getUserId(),
+                order.getOrderProducts().stream().map(OrderProductDto::fromEntity).toList()
+        );
+    }
+}
