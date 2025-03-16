a8a575cedbd99a54a42a49b7425d37fe24bce954
michalgalus <galusmichal@outlook.com>
2025-03-16 23:01:46 +0100

    SCRUM-4 added new fields to ErrorMessages

--- src/main/java/pl/galushop/GaluShop/component/ErrorMessages.java
+++ src/main/java/pl/galushop/GaluShop/component/ErrorMessages.java
@@ -3,6 +3,7 @@ package pl.galushop.GaluShop.component;
 public final class ErrorMessages {
     public static final String USER_OR_EMPLOYEE_NOT_FOUND = "error.userOrEmployeeNotFound";
     public static final String EMAIL_IS_INVALID = "error.invalidEmail";
+    public static final String ERROR = "error";
 
     //Employee
     public static final String EMPLOYEE_NOT_FOUND = "error.employeeNotFound";
