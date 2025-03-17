b86b16d12a41de60a0e5c06026a5857999b24403
michalgalus <galusmichal@outlook.com>
2025-03-17 23:39:03 +0100

    SCRUM-4 added fromEntity to ProductImagesResponse

--- src/main/java/pl/galushop/GaluShop/dto/response/ProductImagesResponse.java
+++ src/main/java/pl/galushop/GaluShop/dto/response/ProductImagesResponse.java
@@ -1,5 +1,14 @@
 package pl.galushop.GaluShop.dto.response;
 
-public record ProductImagesResponse(Long imagesId, String imgSrc, String altImg, Long productId) {
+import pl.galushop.GaluShop.entity.ProductImages;
 
+public record ProductImagesResponse(Long imagesId, String imgSrc, String altImg, Long productId) {
+    public static ProductImagesResponse fromEntity(ProductImages productImages){
+        return new ProductImagesResponse(
+                productImages.getImagesId(),
+                productImages.getImgSrc(),
+                productImages.getAltImg(),
+                productImages.getProduct().getProductId()
+        );
+    }
 }
