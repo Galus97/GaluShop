169c2b534884dc93136a9782d30182453105003d
michalgalus <galusmichal@outlook.com>
2025-03-17 23:20:37 +0100

    SCRUM-4 added fromEntity to ProductResponse

--- src/main/java/pl/galushop/GaluShop/dto/response/ProductResponse.java
+++ src/main/java/pl/galushop/GaluShop/dto/response/ProductResponse.java
@@ -1,9 +1,22 @@
 package pl.galushop.GaluShop.dto.response;
 
 import pl.galushop.GaluShop.dto.OrderProductDto;
+import pl.galushop.GaluShop.entity.Product;
 
 import java.util.List;
 
 public record ProductResponse (Long productId, String productName, String description, Double price,
                                String category, Integer categoryId, List<OrderProductDto> products){
+
+    public static ProductResponse fromEntity(Product product){
+        return new ProductResponse(
+                product.getProductId(),
+                product.getProductName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getCategory(),
+                product.getCategoryId(),
+                product.getOrderProducts().stream().map(OrderProductDto::fromEntity).toList()
+        );
+    }
 }
