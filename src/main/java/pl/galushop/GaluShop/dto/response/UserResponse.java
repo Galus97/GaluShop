7c41bb98630e7aa704dae1ea60e6546c51cec004
michalgalus <galusmichal@outlook.com>
2025-03-17 22:35:45 +0100

    SCRUM-4 added fromEntity to UserResponse

--- src/main/java/pl/galushop/GaluShop/dto/response/UserResponse.java
+++ src/main/java/pl/galushop/GaluShop/dto/response/UserResponse.java
@@ -1,5 +1,18 @@
 package pl.galushop.GaluShop.dto.response;
 
+import pl.galushop.GaluShop.entity.User;
+
 public record UserResponse(Long userId, String firstName, String lastName, String email,
-                            String password, boolean enabled, String emailCode) {
+                           boolean enabled, String emailCode) {
+
+    public static UserResponse fromEntity(User user){
+        return new UserResponse(
+                user.getUserId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.isEnabled(),
+                user.getEmailCode()
+        );
+    }
 }
