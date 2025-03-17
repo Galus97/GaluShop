7d273c6529bd68dedbe06ca8eb26d6ec0e18b2b3
michalgalus <galusmichal@outlook.com>
2025-03-17 12:36:53 +0100

    SCRUM-4 added constructor in WarehouseProductResponse

--- src/main/java/pl/galushop/GaluShop/dto/response/WarehouseProductResponse.java
+++ src/main/java/pl/galushop/GaluShop/dto/response/WarehouseProductResponse.java
@@ -1,4 +1,14 @@
 package pl.galushop.GaluShop.dto.response;
 
-public record WarehouseProductResponse() {
+import pl.galushop.GaluShop.entity.WarehouseProduct;
+
+public record WarehouseProductResponse(Long warehouseProductId, Long productId, Integer quantity) {
+
+    public static WarehouseProductResponse fromEntity(WarehouseProduct warehouseProduct){
+        return new WarehouseProductResponse(
+                warehouseProduct.getWarehouseProductId(),
+                warehouseProduct.getProduct().getProductId(),
+                warehouseProduct.getQuantity()
+        );
+    }
 }
