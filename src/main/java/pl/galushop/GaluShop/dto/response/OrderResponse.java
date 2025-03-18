a704b2f7c875c4bcf3a746ae9bed6c8df4ca385d
michalgalus <galusmichal@outlook.com>
2025-03-18 10:07:11 +0100

    SCRUM-4 used OrderProductResponse in OrderResponse

--- src/main/java/pl/galushop/GaluShop/dto/response/OrderResponse.java
+++ src/main/java/pl/galushop/GaluShop/dto/response/OrderResponse.java
@@ -1,20 +1,19 @@
 package pl.galushop.GaluShop.dto.response;
 
 import pl.galushop.GaluShop.component.OrderStatus;
-import pl.galushop.GaluShop.dto.OrderProductDto;
 import pl.galushop.GaluShop.entity.Order;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record OrderResponse(Long orderId, LocalDateTime localDateTime, OrderStatus status, Long userId, List<OrderProductDto> products) {
+public record OrderResponse(Long orderId, LocalDateTime localDateTime, OrderStatus status, Long userId, List<OrderProductResponse> products) {
     public static OrderResponse fromEntity(Order order) {
         return new OrderResponse(
                 order.getOrderId(),
                 order.getLocalDateTime(),
                 order.getStatus(),
                 order.getUser().getUserId(),
-                order.getOrderProducts().stream().map(OrderProductDto::fromEntity).toList()
+                order.getOrderProducts().stream().map(OrderProductResponse::fromEntity).toList()
         );
     }
 }
