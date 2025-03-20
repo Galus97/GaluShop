aa426e87f013286d2482d6aaee5502ff314856fa
michalgalus <galusmichal@outlook.com>
2025-03-20 22:06:11 +0100

    SCRUM-4 added setUp to WarehouseProductServiceTest

--- src/test/java/pl/galushop/GaluShop/service/WarehouseProductServiceTest.java
+++ src/test/java/pl/galushop/GaluShop/service/WarehouseProductServiceTest.java
@@ -1,7 +1,35 @@
 package pl.galushop.GaluShop.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.parameters.P;
+import pl.galushop.GaluShop.component.MessageService;
+import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.entity.WarehouseProduct;
+import pl.galushop.GaluShop.repository.WarehouseProductRepository;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class WarehouseProductServiceTest {
-
+    @Mock
+    WarehouseProductRepository repository;
+    @Mock
+    ProductService productService;
+    @Mock
+    MessageService messageService;
+    @InjectMocks
+    WarehouseProductService service;
+    private WarehouseProduct warehouseProduct;
+    @BeforeEach
+    void setUp(){
+        warehouseProduct = WarehouseProduct.builder()
+                .warehouseProductId(1L)
+                .product(new Product())
+                .quantity(10)
+                .build();
+    }
 }
