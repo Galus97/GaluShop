377a2083afa360f493dc74bb9ca3538fd1fb3a4b
michalgalus <galusmichal@outlook.com>
2025-03-23 12:41:03 +0100

    SCRUM-4 added setup to UserDataServiceTes

--- src/test/java/pl/galushop/GaluShop/service/UserDataServiceTest.java
+++ src/test/java/pl/galushop/GaluShop/service/UserDataServiceTest.java
@@ -1,11 +1,54 @@
 package pl.galushop.GaluShop.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.galushop.GaluShop.component.MessageService;
+import pl.galushop.GaluShop.entity.User;
+import pl.galushop.GaluShop.entity.UserData;
+import pl.galushop.GaluShop.repository.UserDataRepository;
+import pl.galushop.GaluShop.repository.UserRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserDataServiceTest {
 
+    @Mock
+    UserDataRepository repository;
+    @Mock
+    UserRepository userRepository;
+    @Mock
+    MessageService messageService;
+    @InjectMocks
+    UserDataService service;
+    private UserData userData;
+    private User user;
+
+    @BeforeEach
+    void setup(){
+        user = User.builder()
+                .userId(1L)
+                .firstName("John")
+                .lastName("Smith")
+                .email("john.smith@mail.com")
+                .password("password")
+                .enabled(true)
+                .emailCode("1111")
+                .build();
+
+        userData = UserData.builder()
+                .userDataId(1L)
+                .city("Warsaw")
+                .street("Pulawska")
+                .streetNumber(1)
+                .apartmentNumber(1)
+                .zipCode("00-001")
+                .phoneNumber(1)
+                .user(user)
+                .build();
+
+    }
 }
