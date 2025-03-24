2957d679b46aff808d0da8a5277cf8b410a97b51
michalgalus <galusmichal@outlook.com>
2025-03-24 17:03:50 +0100

    SCRUM-4 added givenCorrectRequest_whenSaveNewUser_thenReturnsUserResponse to RegisterUserServiceTest

--- src/test/java/pl/galushop/GaluShop/service/RegisterUserServiceTest.java
+++ src/test/java/pl/galushop/GaluShop/service/RegisterUserServiceTest.java
@@ -1,11 +1,65 @@
 package pl.galushop.GaluShop.service;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import pl.galushop.GaluShop.component.RegisterValidator;
+import pl.galushop.GaluShop.dto.request.UserRequest;
+import pl.galushop.GaluShop.dto.response.UserResponse;
+import pl.galushop.GaluShop.entity.User;
+import pl.galushop.GaluShop.repository.UserRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RegisterUserServiceTest {
+    @Mock
+    UserRepository repository;
+    @Mock
+    PasswordEncoder passwordEncoder;
+    @Mock
+    RegisterValidator registerValidator;
+    @Mock
+    EmailService emailService;
+    @InjectMocks
+    RegisterUserService service;
 
+    @Test
+    void givenCorrectRequest_whenSaveNewUser_thenReturnsUserResponse() throws Exception{
+        //given
+        UserRequest userRequest = new UserRequest();
+        userRequest.setUserId(null);
+        userRequest.setFirstName("John");
+        userRequest.setLastName("Smith");
+        userRequest.setEmail("john.smith@mail.com");
+        userRequest.setPassword("password");
+
+        User user = User.builder()
+                .userId(1L)
+                .firstName("John")
+                .lastName("Smith")
+                .email("john.smith@mail.com")
+                .password("password")
+                .enabled(true)
+                .emailCode("1111")
+                .build();
+        when(repository.save(any(User.class))).thenReturn(user);
+        //when
+        UserResponse response = service.saveNewUser(userRequest);
+        //then
+        assertNotNull(response);
+        assertEquals("John", response.firstName());
+        assertEquals("Smith", response.lastName());
+        assertEquals("john.smith@mail.com", response.email());
+        assertEquals("1111", response.emailCode());
+        assertTrue(response.enabled());
+        verify(repository, times(1)).save(any(User.class));
+    }
 }
