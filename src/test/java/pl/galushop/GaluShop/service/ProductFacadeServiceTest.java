167e9acf730d68b1efc5c4db426f3f45aba84727
michalgalus <galusmichal@outlook.com>
2025-03-25 19:40:25 +0100

    SCRUM-4 added givenCorrectRequest_whenSaveProductWithImages_thenReturnsProductResponse to ProductFacadeServiceTest

--- src/test/java/pl/galushop/GaluShop/service/ProductFacadeServiceTest.java
+++ src/test/java/pl/galushop/GaluShop/service/ProductFacadeServiceTest.java
@@ -1,11 +1,61 @@
 package pl.galushop.GaluShop.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import pl.galushop.GaluShop.dto.request.ProductImageRequest;
+import pl.galushop.GaluShop.dto.request.ProductRequest;
+import pl.galushop.GaluShop.dto.response.ProductImagesResponse;
+import pl.galushop.GaluShop.dto.response.ProductResponse;
+import pl.galushop.GaluShop.entity.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ProductFacadeServiceTest {
+    @Mock
+    private ProductService productService;
+    @Mock
+    private ProductImagesService productImagesService;
+    @InjectMocks
+    private ProductFacadeService productFacadeService;
+
+    private Product product;
+    private ProductRequest productRequest;
+    private ProductImageRequest productImageRequest;
+
+    @BeforeEach
+    void setUp() {
+        product = new Product();
+        product.setProductId(1L);
+        product.setOrderProducts(new ArrayList<>());
+
+        productImageRequest = new ProductImageRequest();
+        productRequest = new ProductRequest();
+        productRequest.setProductImages(Collections.singletonList(productImageRequest));
+    }
+
+    @Test
+    void givenCorrectRequest_whenSaveProductWithImages_thenReturnsProductResponse() {
+        //given
+        when(productService.saveProductEntity(any(ProductRequest.class))).thenReturn(product);
+        //when
+        ProductResponse response = productFacadeService.saveProductWithImages(productRequest);
+        //then
+        assertNotNull(response);
+        assertEquals(1L, response.productId());
+        verify(productImagesService, times(1)).saveProductImages(any(ProductImageRequest.class));
+    }
 
 }
