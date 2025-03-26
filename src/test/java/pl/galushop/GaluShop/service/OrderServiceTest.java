bb79d9b78abd2795bc5476defb2a0db2cbb30328
michalgalus <galusmichal@outlook.com>
2025-03-26 15:49:12 +0100

    SCRUM-4 added givenExistingId_whenGetOrderResponse_thenReturnsOrderResponse to OrderServiceTest

--- src/test/java/pl/galushop/GaluShop/service/OrderServiceTest.java
+++ src/test/java/pl/galushop/GaluShop/service/OrderServiceTest.java
@@ -1,6 +1,7 @@
 package pl.galushop.GaluShop.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -9,6 +10,7 @@ import pl.galushop.GaluShop.component.MessageService;
 import pl.galushop.GaluShop.component.OrderStatus;
 import pl.galushop.GaluShop.dto.request.OrderRequest;
 import pl.galushop.GaluShop.dto.request.ProductQuantityRequest;
+import pl.galushop.GaluShop.dto.response.OrderResponse;
 import pl.galushop.GaluShop.entity.Order;
 import pl.galushop.GaluShop.entity.OrderProduct;
 import pl.galushop.GaluShop.entity.Product;
@@ -18,8 +20,13 @@ import pl.galushop.GaluShop.repository.OrderRepository;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -62,5 +69,19 @@ class OrderServiceTest {
         orderRequest.setLocalDateTime(LocalDateTime.of(2025, 3, 26, 12,12));
         orderRequest.setOrderStatus(OrderStatus.PROCESSED);
         orderRequest.setProductQuantityRequests(productQuantityRequests);
+    }
+
+    @Test
+    void givenExistingId_whenGetOrderResponse_thenReturnsOrderResponse(){
+        //given
+        when(repository.findById(anyLong())).thenReturn(Optional.of(order));
+        //when
+        OrderResponse response = service.getOrderResponse(1L);
+        //then
+        assertNotNull(response);
+        assertEquals(1L, response.orderId());
+        assertEquals(OrderStatus.PROCESSED, response.status());
+        assertEquals(1L, response.userId());
+        verify(repository, times(1)).findById(anyLong());
     }
 }
