ae90f10593790102b19f9e705cad21b41f95e10c
michalgalus <galusmichal@outlook.com>
2025-03-28 13:12:38 +0100

    SCRUM-4 added givenNonExistingProductId_whenGetAllImagesFromProduct_thenReturnsEmptyList to AllProductImagesControllerTest

--- src/test/java/pl/galushop/GaluShop/controller/AllProductImagesControllerTest.java
+++ src/test/java/pl/galushop/GaluShop/controller/AllProductImagesControllerTest.java
@@ -12,12 +12,15 @@ import pl.galushop.GaluShop.dto.response.ProductImagesResponse;
 import pl.galushop.GaluShop.service.ProductFacadeService;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -47,5 +50,17 @@ class AllProductImagesControllerTest {
                 .andExpect(jsonPath("$[0].imgSrc", is("Img Src")))
                 .andExpect(jsonPath("$[0].altImg", is("Alt Img")))
                 .andExpect(jsonPath("$[0].productId", is(1)));
+            verify(productFacadeService, times(1)).getAllImagesByProductId(eq(1L));
+    }
+
+    @Test
+    void givenNonExistingProductId_whenGetAllImagesFromProduct_thenReturnsEmptyList() throws Exception {
+        //given
+        when(productFacadeService.getAllImagesByProductId(anyLong())).thenReturn(Collections.emptyList());
+        //then
+        mockMvc.perform(get("/images/product/999"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(0)));
+        verify(productFacadeService, times(1)).getAllImagesByProductId(eq(999L));
     }
 }
--- src/test/java/pl/galushop/GaluShop/controller/EmployeeControllerTest.java
+++ src/test/java/pl/galushop/GaluShop/controller/EmployeeControllerTest.java
@@ -1,10 +1,28 @@
 package pl.galushop.GaluShop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.galushop.GaluShop.configuration.SpringSecurity;
+import pl.galushop.GaluShop.service.EmployeeService;
+import pl.galushop.GaluShop.service.RegisterEmployeeService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(EmployeeController.class)
+@Import(SpringSecurity.class)
+@AutoConfigureMockMvc(addFilters = false)
 class EmployeeControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+    @MockBean
+    EmployeeService employeeService;
+    @MockBean
+    RegisterEmployeeService registerEmployeeService;
+
 
 }
