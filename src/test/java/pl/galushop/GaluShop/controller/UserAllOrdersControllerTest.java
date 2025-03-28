cfc96df8f3458dcd9b60dc9d2320e9a28d4cbdb7
michalgalus <galusmichal@outlook.com>
2025-03-28 13:21:11 +0100

    SCRUM-4 added givenNonExistent_whenShowAllUserOrders_thenReturnsEmptyList to UserAllOrdersControllerTest

--- src/test/java/pl/galushop/GaluShop/controller/UserAllOrdersControllerTest.java
+++ src/test/java/pl/galushop/GaluShop/controller/UserAllOrdersControllerTest.java
@@ -15,6 +15,7 @@ import pl.galushop.GaluShop.dto.response.OrderResponse;
 import pl.galushop.GaluShop.service.OrderService;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -23,8 +24,8 @@ import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,9 +41,9 @@ class UserAllOrdersControllerTest {
     OrderService orderService;
 
     @Test
-    void givenExistingId_whenShowAllUserOrders_thenReturnsResponseEntityWithOrderResponseResponseList() throws Exception{
+    void givenExistingId_whenShowAllUserOrders_thenReturnsResponseEntityWithOrderResponseResponseList() throws Exception {
         //given
-        List<OrderProductResponse> OrderProductResponseList = List.of( new OrderProductResponse(1L, 1L, 10));
+        List<OrderProductResponse> OrderProductResponseList = List.of(new OrderProductResponse(1L, 1L, 10));
         List<OrderResponse> orderResponseList = List.of(new OrderResponse(
                 1L,
                 LocalDateTime.of(2025, 3, 13, 12, 12, 12),
@@ -63,5 +64,17 @@ class UserAllOrdersControllerTest {
                 .andExpect(jsonPath("$[0].products[0].productId", is(1)))
                 .andExpect(jsonPath("$[0].products[0].quantity", is(10)));
         verify(orderService, times(1)).getAllOrdersByUser(eq(1L));
+    }
+
+    @Test
+    void givenNonExistent_whenShowAllUserOrders_thenReturnsEmptyList() throws Exception {
+        //given
+        when(orderService.getAllOrdersByUser(anyLong())).thenReturn(Collections.emptyList());
+        //then
+        mockMvc.perform(get("/order/user/999"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(0)));
+        verify(orderService, times(1)).getAllOrdersByUser(eq(999L));
     }
 }
