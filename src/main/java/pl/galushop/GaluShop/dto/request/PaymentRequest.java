d13535c4172cfe279cd220d6cd96222c033f87d3
michalgalus <galusmichal@outlook.com>
2025-04-01 00:06:28 +0200

    SCRUM-4 added Builder to PaymentRequest

--- src/main/java/pl/galushop/GaluShop/dto/request/PaymentRequest.java
+++ src/main/java/pl/galushop/GaluShop/dto/request/PaymentRequest.java
@@ -1,9 +1,11 @@
 package pl.galushop.GaluShop.dto.request;
 
+import lombok.Builder;
 import lombok.Data;
 import pl.galushop.GaluShop.component.PaymentStatus;
 
 @Data
+@Builder
 public class PaymentRequest {
     private Long paymentId;
     private Double totalAmount;
--- src/test/java/pl/galushop/GaluShop/controller/PaymentsControllerTest.java
+++ src/test/java/pl/galushop/GaluShop/controller/PaymentsControllerTest.java
@@ -1,14 +1,30 @@
 package pl.galushop.GaluShop.controller;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
 import pl.galushop.GaluShop.configuration.SpringSecurity;
+import pl.galushop.GaluShop.dto.request.PaymentRequest;
+import pl.galushop.GaluShop.dto.response.PaymentResponse;
+import pl.galushop.GaluShop.service.PaymentService;
 
-import static org.junit.jupiter.api.Assertions.*;
 @WebMvcTest(PaymentsController.class)
 @Import(SpringSecurity.class)
 @AutoConfigureMockMvc(addFilters = false)
 class PaymentsControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    PaymentService paymentService;
+    private PaymentResponse response;
+    private PaymentRequest request;
 
+    @BeforeEach
+    void setUp(){
+        //response
+    }
 }
