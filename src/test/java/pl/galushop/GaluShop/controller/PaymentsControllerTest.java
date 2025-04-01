ace22fd431ef99973eab850a68c7bbe61af48261
michalgalus <galusmichal@outlook.com>
2025-04-01 16:29:24 +0200

    SCRUM-4 added givenExistingId_whenDeletePayment_thenDeletesPayment to PaymentsControllerTest

--- src/test/java/pl/galushop/GaluShop/controller/PaymentsControllerTest.java
+++ src/test/java/pl/galushop/GaluShop/controller/PaymentsControllerTest.java
@@ -3,6 +3,7 @@ package pl.galushop.GaluShop.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -19,9 +20,12 @@ import pl.galushop.GaluShop.service.PaymentService;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -127,5 +131,15 @@ class PaymentsControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.paymentId").value(1L));
         verify(service, times(1)).updatePayment(request);
+    }
+
+    @Test
+    void givenExistingId_whenDeletePayment_thenDeletesPayment() throws Exception{
+        //given
+        doNothing().when(service).deletePayment(anyLong());
+        //then
+        mockMvc.perform(delete("/payments/1"))
+                .andExpect(status().isNoContent());
+        verify(service, times(1)).deletePayment(1L);
     }
 }
