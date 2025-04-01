e08293a235d8c51be7a8d38213ac5d37f806a989
michalgalus <galusmichal@outlook.com>
2025-04-01 22:59:17 +0200

    SCRUM-4 added UserControllerTest.java

---
+++ src/test/java/pl/galushop/GaluShop/controller/UserControllerTest.java
@@ -0,0 +1,14 @@
+package pl.galushop.GaluShop.controller;
+
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import pl.galushop.GaluShop.configuration.SpringSecurity;
+
+@WebMvcTest(UserController.class)
+@Import(SpringSecurity.class)
+@AutoConfigureMockMvc(addFilters = false)
+class UserControllerTest {
+
+}
