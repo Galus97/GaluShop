83ffcf368e453c58336491282496e57c92c841f2
michalgalus <galusmichal@outlook.com>
2025-04-03 13:26:34 +0200

    SCRUM-4 added WarehouseControllerTest.java

--- src/main/java/pl/galushop/GaluShop/controller/WarehouseController.java
+++ src/main/java/pl/galushop/GaluShop/controller/WarehouseController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.galushop.GaluShop.dto.request.WarehouseProductRequest;
 import pl.galushop.GaluShop.dto.response.WarehouseProductResponse;
-import pl.galushop.GaluShop.entity.WarehouseProduct;
 import pl.galushop.GaluShop.service.WarehouseProductService;
 
 import java.net.URI;
