c26a4b5e69b91eb7a361b0c0e1b37690c865d8d7
michalgalus <galusmichal@outlook.com>
2025-04-04 11:15:15 +0200

    SCRUM-4 added givenExistingId_whenShowWarehouseProduct_thenReturnsWarehouseProduct to WarehouseControllerTest

--- src/test/java/pl/galushop/GaluShop/controller/WarehouseControllerTest.java
+++ src/test/java/pl/galushop/GaluShop/controller/WarehouseControllerTest.java
@@ -2,16 +2,27 @@ package pl.galushop.GaluShop.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import pl.galushop.GaluShop.configuration.SpringSecurity;
 import pl.galushop.GaluShop.dto.request.WarehouseProductRequest;
 import pl.galushop.GaluShop.dto.response.WarehouseProductResponse;
 import pl.galushop.GaluShop.service.WarehouseProductService;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WarehouseController.class)
 @Import(SpringSecurity.class)
@@ -23,7 +34,7 @@ class WarehouseControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
-    private WarehouseProductService warehouseProductService;
+    private WarehouseProductService service;
     private WarehouseProductResponse response;
     private WarehouseProductRequest request;
 
@@ -35,5 +46,19 @@ class WarehouseControllerTest {
                 .productId(1L)
                 .quantity(10)
                 .build();
+    }
+
+    @Test
+    void givenExistingId_whenShowWarehouseProduct_thenReturnsWarehouseProduct() throws Exception{
+        //given
+        when(service.getWarehouseProductResponse(anyLong())).thenReturn(response);
+        //then
+        mockMvc.perform(get("/warehouse/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.warehouseProductId").value(1L))
+                .andExpect(jsonPath("$.productId").value(1L))
+                .andExpect(jsonPath("$.quantity").value(10));
+        verify(service, times(1)).getWarehouseProductResponse(1L);
     }
 }
