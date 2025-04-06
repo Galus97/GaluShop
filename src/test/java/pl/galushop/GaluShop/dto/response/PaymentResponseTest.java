f410de4796ca4f62c771c730249b0fbb343a2459
michalgalus <galusmichal@outlook.com>
2025-04-06 17:59:47 +0200

    SCRUM-4 added givenPayment_whenFromEntity_thenReturnsCorrectResponse to PaymentResponseTest

--- src/test/java/pl/galushop/GaluShop/dto/response/PaymentResponseTest.java
+++ src/test/java/pl/galushop/GaluShop/dto/response/PaymentResponseTest.java
@@ -1,7 +1,37 @@
 package pl.galushop.GaluShop.dto.response;
 
+import org.junit.jupiter.api.Test;
+import pl.galushop.GaluShop.component.PaymentStatus;
+import pl.galushop.GaluShop.entity.Order;
+import pl.galushop.GaluShop.entity.Payment;
+import pl.galushop.GaluShop.entity.User;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentResponseTest {
 
+    @Test
+    void givenPayment_whenFromEntity_thenReturnsCorrectResponse(){
+        //given
+        Order order = new Order();
+        order.setOrderId(1L);
+        User user = new User();
+        user.setUserId(1L);
+
+        Payment payment = Payment.builder()
+                .paymentId(1L)
+                .totalAmount(10d)
+                .paymentStatus(PaymentStatus.NEW)
+                .order(order)
+                .user(user)
+                .build();
+        //when
+        PaymentResponse response = PaymentResponse.fromEntity(payment);
+        //then
+        assertEquals(1L, response.paymentId());
+        assertEquals(10.0, response.totalAmount());
+        assertEquals(PaymentStatus.NEW, response.paymentStatus());
+        assertEquals(1L, response.orderId());
+        assertEquals(1L, response.userId());
+    }
 }
