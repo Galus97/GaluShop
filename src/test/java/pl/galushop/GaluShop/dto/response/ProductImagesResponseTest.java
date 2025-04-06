0dc05910bb5f86980602d59f26a1f74d0a24b43f
michalgalus <galusmichal@outlook.com>
2025-04-06 18:06:17 +0200

    SCRUM-4 added givenProductImages_whenFromEntity_thenReturnsCorrectRequest to ProductImagesResponseTest

--- src/test/java/pl/galushop/GaluShop/dto/response/ProductImagesResponseTest.java
+++ src/test/java/pl/galushop/GaluShop/dto/response/ProductImagesResponseTest.java
@@ -1,7 +1,31 @@
 package pl.galushop.GaluShop.dto.response;
 
+import org.junit.jupiter.api.Test;
+import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.entity.ProductImages;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductImagesResponseTest {
 
+    @Test
+    void givenProductImages_whenFromEntity_thenReturnsCorrectRequest(){
+        //given
+        Product product = new Product();
+        product.setProductId(1L);
+
+        ProductImages productImages = ProductImages.builder()
+                .imagesId(1L)
+                .imgSrc("Img src")
+                .altImg("Img alt")
+                .product(product)
+                .build();
+        //when
+        ProductImagesResponse response = ProductImagesResponse.fromEntity(productImages);
+        //then
+        assertEquals(1L, response.imagesId());
+        assertEquals("Img src", response.imgSrc());
+        assertEquals("Img alt", response.altImg());
+        assertEquals(1L, response.productId());
+    }
 }
