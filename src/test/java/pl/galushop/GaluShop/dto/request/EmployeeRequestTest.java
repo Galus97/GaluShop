1a41bdc4fa0428be7db75d4da7490bbdc36bd866
michalgalus <galusmichal@outlook.com>
2025-04-06 17:37:01 +0200

    SCRUM-4 added EmployeeRequestTest.java

---
+++ src/test/java/pl/galushop/GaluShop/dto/request/EmployeeRequestTest.java
@@ -0,0 +1,11 @@
+package pl.galushop.GaluShop.dto.request;
+
+import org.junit.jupiter.api.Test;
+import pl.galushop.GaluShop.dto.response.EmployeeResponse;
+import pl.galushop.GaluShop.entity.Employee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeRequestTest {
+    
+}
