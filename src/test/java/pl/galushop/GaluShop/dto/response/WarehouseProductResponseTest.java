d13c9b9ecd61be62427d0e007d7e8aeb7bbada8a
michalgalus <galusmichal@outlook.com>
2025-04-07 17:56:10 +0200

    SCRUM-4 added givenWarehouseProduct_whenFromEntity_thenReturnsWarehouseProductResponse to WarehouseProductResponseTest

--- src/test/java/pl/galushop/GaluShop/dto/response/WarehouseProductResponseTest.java
+++ src/test/java/pl/galushop/GaluShop/dto/response/WarehouseProductResponseTest.java
@@ -1,7 +1,29 @@
 package pl.galushop.GaluShop.dto.response;
 
+import org.junit.jupiter.api.Test;
+import pl.galushop.GaluShop.entity.Product;
+import pl.galushop.GaluShop.entity.WarehouseProduct;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WarehouseProductResponseTest {
 
+    @Test
+    void givenWarehouseProduct_whenFromEntity_thenReturnsWarehouseProductResponse(){
+        //given
+        Product product = new Product();
+        product.setProductId(1L);
+
+        WarehouseProduct warehouseProduct = WarehouseProduct.builder()
+                .warehouseProductId(1L)
+                .product(product)
+                .quantity(10)
+                .build();
+        //when
+        WarehouseProductResponse response = WarehouseProductResponse.fromEntity(warehouseProduct);
+        //then
+        assertEquals(1L, response.warehouseProductId());
+        assertEquals(1L, response.productId());
+        assertEquals(10, response.quantity());
+    }
 }
