79e8fac7ce256daf62becd22bad7c956902ddcca
michalgalus <galusmichal@outlook.com>
2025-04-13 19:56:27 +0200

    SCRUM-5 added JavaMailSender as MockBean to GaluShopApplicationTests

--- src/test/java/pl/galushop/GaluShop/GaluShopApplicationTests.java
+++ src/test/java/pl/galushop/GaluShop/GaluShopApplicationTests.java
@@ -2,11 +2,16 @@ package pl.galushop.GaluShop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class GaluShopApplicationTests {
+
+	@MockBean
+	private JavaMailSender javaMailSender;
 
 	@Test
 	void contextLoads() {
