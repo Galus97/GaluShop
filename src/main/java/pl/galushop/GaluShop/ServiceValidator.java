edf49c1b936d6788bb1d904db108c5001a1d764d
michalgalus <galusmichal@outlook.com>
2026-07-03 17:55:59 +0200

    SCRUM-7 added throwIfRequestIsNull to ServiceValidator

--- src/main/java/pl/galushop/GaluShop/ServiceValidator.java
+++ src/main/java/pl/galushop/GaluShop/ServiceValidator.java
@@ -2,8 +2,16 @@ package pl.galushop.GaluShop;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import pl.galushop.GaluShop.component.MessageService;
 
 @Component
 @RequiredArgsConstructor
 public class ServiceValidator {
+    private final MessageService messageService;
+
+    public void throwIfRequestIsNull(Object request, String errorMessageKey) {
+        if (request == null) {
+            throw new IllegalArgumentException(messageService.getMessage(errorMessageKey));
+        }
+    }
 }
