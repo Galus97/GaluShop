4d761f729e3c48354711d4c1dc50cdd7b60575fe
michalgalus <galusmichal@outlook.com>
2026-07-03 18:03:48 +0200

    SCRUM-7 deleted throwIfEmailIsInvalid method from ServiceValidator

--- src/main/java/pl/galushop/GaluShop/service/EmailService.java
+++ src/main/java/pl/galushop/GaluShop/service/EmailService.java
@@ -35,7 +35,6 @@ public class EmailService {
      */
     @Async
     public void sendEmail(String email) {
-        throwIfEmailIsInvalid(email);
         String emailActiveCode = generateActiveCode();
         cacheManager.getCache(ErrorMessages.VERIFICATION_CODE).put(email, emailActiveCode);
 
@@ -71,17 +70,5 @@ public class EmailService {
     private String generateActiveCode() {
         Random random = new Random();
         return String.valueOf(random.nextInt(1000, 9999));
-    }
-
-    /**
-     * Validates that the given email is not {@code null} or blank.
-     *
-     * @param email The email address to validate.
-     * @throws IllegalArgumentException If the email is {@code null} or blank.
-     */
-    private void throwIfEmailIsInvalid(String email) {
-        if (email == null || email.isBlank()) {
-            throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.EMAIL_IS_INVALID, email));
-        }
     }
 }
