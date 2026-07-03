371f2dca5fbec59b5a5c906bdd3480ecab3459df
michalgalus <galusmichal@outlook.com>
2026-07-03 17:55:37 +0200

    SCRUM-7 added ServiceValidator

---
+++ src/main/java/pl/galushop/GaluShop/ServiceValidator.java
@@ -0,0 +1,9 @@
+package pl.galushop.GaluShop;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ServiceValidator {
+}
