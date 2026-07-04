ca1a0d3f923de0cd81d512979da48571de3e2075
michalgalus <galusmichal@outlook.com>
2026-07-04 15:54:00 +0200

    SCRUM-7 added serviceValidator.throwIfIdIsNotValid to getPaymentResponse

--- src/main/java/pl/galushop/GaluShop/service/PaymentService.java
+++ src/main/java/pl/galushop/GaluShop/service/PaymentService.java
@@ -3,6 +3,7 @@ package pl.galushop.GaluShop.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.galushop.GaluShop.ServiceValidator;
 import pl.galushop.GaluShop.component.ErrorMessages;
 import pl.galushop.GaluShop.component.MessageService;
 import pl.galushop.GaluShop.dto.request.PaymentRequest;
@@ -30,6 +31,7 @@ public class PaymentService {
     private final MessageService messageService;
     private final OrderService orderService;
     private final UserService userService;
+    private final ServiceValidator serviceValidator;
 
     /**
      * Retrieves a payment by its ID.
@@ -40,8 +42,7 @@ public class PaymentService {
      * @throws PaymentNotFoundException if no payment is found with the given ID.
      */
     public PaymentResponse getPaymentResponse(Long paymentId) {
-        throwIfIdIsInvalid(paymentId, ErrorMessages.INVALID_PAYMENT_ID);
-
+        serviceValidator.throwIfIdIsNotValid(paymentId, ErrorMessages.INVALID_PAYMENT_ID);
         return PaymentResponse.fromEntity(getPaymentOrThrow(paymentId, ErrorMessages.PAYMENT_NOT_FOUND));
     }
 
@@ -151,31 +152,31 @@ public class PaymentService {
                 .user(user)
                 .build();
     }
-
-    /**
-     * Validates that the given PaymentRequest is not null.
-     *
-     * @param paymentRequest The request to validate.
-     * @throws IllegalArgumentException if the request is null.
-     */
-    private void throwIfRequestIsNull(PaymentRequest paymentRequest) {
-        if (paymentRequest == null) {
-            throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.PAYMENT_REQUEST_IS_NULL));
-        }
-    }
-
-    /**
-     * Validates that the given ID is not null or less than or equal to zero.
-     *
-     * @param id      The ID to validate.
-     * @param message The message key used if the validation fails.
-     * @throws IllegalArgumentException if the ID is null or invalid.
-     */
-    private void throwIfIdIsInvalid(Long id, String message) {
-        if (id == null || id <= 0) {
-            throw new IllegalArgumentException(messageService.getMessage(message, id));
-        }
-    }
+//
+//    /**
+//     * Validates that the given PaymentRequest is not null.
+//     *
+//     * @param paymentRequest The request to validate.
+//     * @throws IllegalArgumentException if the request is null.
+//     */
+//    private void throwIfRequestIsNull(PaymentRequest paymentRequest) {
+//        if (paymentRequest == null) {
+//            throw new IllegalArgumentException(messageService.getMessage(ErrorMessages.PAYMENT_REQUEST_IS_NULL));
+//        }
+//    }
+//
+//    /**
+//     * Validates that the given ID is not null or less than or equal to zero.
+//     *
+//     * @param id      The ID to validate.
+//     * @param message The message key used if the validation fails.
+//     * @throws IllegalArgumentException if the ID is null or invalid.
+//     */
+//    private void throwIfIdIsInvalid(Long id, String message) {
+//        if (id == null || id <= 0) {
+//            throw new IllegalArgumentException(messageService.getMessage(message, id));
+//        }
+//    }
 
     /**
      * Retrieves a Payment entity by ID or throws an exception if not found.
